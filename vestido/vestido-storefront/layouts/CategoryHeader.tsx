276fc0aa43263ed04f69df6e6d921eb2c34da6a3
pvsameerpvs <pvsameerpvs@gmail.com>
2024-02-03 10:02:44 +0530

    fix: Bugs Are Fix

--- vestido/vestido-storefront/layouts/CategoryHeader.tsx
+++ vestido/vestido-storefront/layouts/CategoryHeader.tsx
@@ -1,14 +1,10 @@
 import * as React from 'react';
-import Link from 'next/link';
-
 import {
   NavigationMenu,
   NavigationMenuContent,
   NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
   NavigationMenuTrigger,
-  navigationMenuTriggerStyle,
+  NavigationMenuList,
 } from '@vestido-ecommerce/shadcn-ui/navigation-menu';
 import { Search } from 'lucide-react';
 import { Input } from '@vestido-ecommerce/shadcn-ui/input';
@@ -16,156 +12,119 @@ import { Input } from '@vestido-ecommerce/shadcn-ui/input';
 type ListItemProps = {
   href: string;
   title: string;
-  children: React.ReactNode;
 };
 
-const ListItem: React.FC<ListItemProps> = ({ href, title, children }) => (
-  <li>
-    <NavigationMenuLink asChild>
-      <a
-        className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-        href={href}
-      >
-        <div className="mb-2 mt-4 text-lg font-medium">{title}</div>
-        <p className="text-sm leading-tight text-muted-foreground">
-          {children}
-        </p>
-      </a>
-    </NavigationMenuLink>
+const ListItem: React.FC<ListItemProps> = ({ href, title }) => (
+  <li className="row-span-3">
+    <a
+      className="flex select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-3 no-underline outline-none focus:shadow-md"
+      href={href}
+    >
+      <div className="text-sm font-medium p-1">{title}</div>
+    </a>
   </li>
 );
 
-const mens = [
+const categoriesData = [
   {
-    title: 'Top Wear',
-    href: '/docs/primitives/alert-dialog',
-    categoryTile: (
-      <ul>
-        <li>T-shirt</li>
-        <li>Shirt</li>
-        <li>Jacket</li>
-      </ul>
-    ),
+    category: "MEN'S",
+    subcategories: [
+      {
+        title: 'Top Wear',
+        items: [
+          'Casual Shirts',
+          'Formal Shirts',
+          'Denim Shirts',
+          'T Shirts',
+          'Hoodies',
+        ],
+      },
+      {
+        title: 'Bottom Wear',
+        items: [
+          'Baggy Jeans',
+          'Casual Jeans',
+          'Cargo Pants',
+          'Formal Pants',
+          'Jogger',
+        ],
+      },
+    ],
   },
   {
-    title: 'Bottom Wear',
-    href: '/docs/primitives/alert-dialog',
-    categoryTile: (
-      <ul>
-        <li>Pant</li>
-        <li>Inner</li>
-        <li>Trouser</li>
-      </ul>
-    ),
+    category: "WOMEN'S",
+    subcategories: [
+      {
+        title: 'Dresses',
+        items: [
+          'A-Line Dress',
+          'Floaral Dress',
+          'Bodycon Dress',
+          'Cocktail Dress',
+          'Casual Dress',
+        ],
+      },
+      {
+        title: 'Other',
+        items: ['Kurthi Dress', 'Wrap Dress', 'Salwar Dress'],
+      },
+    ],
+  },
+  {
+    category: 'KIDS',
+    subcategories: [
+      {
+        title: 'Boys',
+        items: [
+          'Casual Shirts',
+          'Formal Shirts',
+          'Denim Shirts',
+          'T Shirts',
+          'Hoodies',
+        ],
+      },
+      {
+        title: 'Girls',
+        items: [
+          'A-Line Dress',
+          'Floaral Dress',
+          'Bodycon Dress',
+          'Cocktail Dress',
+          'Casual Dress',
+        ],
+      },
+    ],
   },
 ];
-const womens = [
-  {
-    title: 'Top Wear',
-    href: '/docs/primitives/alert-dialog',
-    categoryTile: (
-      <ul>
-        <li>Churidar</li>
-        <li>Blouse</li>
-        <li>Saree</li>
-      </ul>
-    ),
-  },
-  {
-    title: 'Bottom Wear',
-    href: '/docs/primitives/alert-dialog',
-    categoryTile: (
-      <ul>
-        <li>Pant</li>
-        <li>Inner</li>
-        <li>Partywear</li>
-      </ul>
-    ),
-  },
-];
-const kids = [
-  {
-    title: 'Top Wear',
-    href: '/docs/primitives/alert-dialog',
-    categoryTile: (
-      <ul>
-        <li>Boys</li>
-      </ul>
-    ),
-  },
-  {
-    title: 'Bottom Wear',
-    href: '/docs/primitives/alert-dialog',
-    categoryTile: (
-      <ul>
-        <li>Girls</li>
-      </ul>
-    ),
-  },
-];
-
-const categories = ["WOMEN'S", "MEN'S", 'KIDS'];
 
 const CategoryHeader = () => {
-  const [selectedCategory, setSelectedCategory] = React.useState('WOMEN');
-
-  const handleCategoryChange = (category: string) => {
-    setSelectedCategory(category);
-  };
-
-  const getCategoryData = () => {
-    switch (selectedCategory) {
-      case "MEN'S":
-        return mens;
-      case "WOMEN'S":
-        return womens;
-      case 'KIDS':
-        return kids;
-      default:
-        return [];
-    }
-  };
-
-  const currentCategoryData = getCategoryData();
-
   return (
-    <div className="flex justify-between border-b border-gray-500 px-16">
-      <div className="flex justify-between items-center py-2">
-        <NavigationMenu className="px-4">
-          <NavigationMenuList className="flex space-x-4">
-            <NavigationMenuItem>
-              <Link href="/docs" legacyBehavior passHref>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                  HOME
-                </NavigationMenuLink>
-              </Link>
+    <div className="flex justify-between border-b border-gray-500 px-16 p-3">
+      <NavigationMenu>
+        <NavigationMenuList>
+          {categoriesData.map((category, index) => (
+            <NavigationMenuItem key={index}>
+              <NavigationMenuTrigger className="font-black px-4">
+                {category.category}
+              </NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <div className="flex gap-2 p-3">
+                  {category.subcategories.map((subcategory, subIndex) => (
+                    <div key={subIndex}>
+                      <h1 className="font-black px-4">{subcategory.title}</h1>
+                      <ul className="grid gap-1 md:w-[200px] lg:w-[200px]">
+                        {subcategory.items.map((item, itemIndex) => (
+                          <ListItem key={itemIndex} href="/" title={item} />
+                        ))}
+                      </ul>
+                    </div>
+                  ))}
+                </div>
+              </NavigationMenuContent>
             </NavigationMenuItem>
-
-            {categories.map((category) => (
-              <NavigationMenuItem key={category}>
-                <NavigationMenuTrigger
-                  onClick={() => handleCategoryChange(category)}
-                >
-                  {category}
-                </NavigationMenuTrigger>
-                <NavigationMenuContent>
-                  <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
-                    {currentCategoryData.map((component) => (
-                      <ListItem
-                        key={component.title}
-                        title={component.title}
-                        href={component.href}
-                      >
-                        {component.categoryTile}
-                      </ListItem>
-                    ))}
-                  </ul>
-                </NavigationMenuContent>
-              </NavigationMenuItem>
-            ))}
-          </NavigationMenuList>
-        </NavigationMenu>
-      </div>
+          ))}
+        </NavigationMenuList>
+      </NavigationMenu>
       <div className="flex space-x-4 items-center justify-items-center content-center">
         <Search />
         <Input type="search" placeholder="Search" />
