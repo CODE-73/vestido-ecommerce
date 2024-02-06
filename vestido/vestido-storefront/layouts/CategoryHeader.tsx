0d2df4d3d25cd2fe821689826a83963d72762e6c
Mohammed Sameer PV <64373702+pvsameerpvs@users.noreply.github.com>
2024-02-06 16:21:06 +0530

    feat: UI Navigation Bar (#1)
    * feat: Creat Header
    
    * fix: Bugs Are Fix
    
    * fix: Bugs Are Fix

---
+++ vestido/vestido-storefront/layouts/CategoryHeader.tsx
@@ -0,0 +1,136 @@
+import * as React from 'react';
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuTrigger,
+  NavigationMenuList,
+} from '@vestido-ecommerce/shadcn-ui/navigation-menu';
+import { Search } from 'lucide-react';
+import { Input } from '@vestido-ecommerce/shadcn-ui/input';
+
+type ListItemProps = {
+  href: string;
+  title: string;
+};
+
+const ListItem: React.FC<ListItemProps> = ({ href, title }) => (
+  <li className="row-span-3">
+    <a
+      className="flex select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-3 no-underline outline-none focus:shadow-md"
+      href={href}
+    >
+      <div className="text-sm font-medium p-1">{title}</div>
+    </a>
+  </li>
+);
+
+const categoriesData = [
+  {
+    category: "MEN'S",
+    subcategories: [
+      {
+        title: 'Top Wear',
+        items: [
+          'Casual Shirts',
+          'Formal Shirts',
+          'Denim Shirts',
+          'T Shirts',
+          'Hoodies',
+        ],
+      },
+      {
+        title: 'Bottom Wear',
+        items: [
+          'Baggy Jeans',
+          'Casual Jeans',
+          'Cargo Pants',
+          'Formal Pants',
+          'Jogger',
+        ],
+      },
+    ],
+  },
+  {
+    category: "WOMEN'S",
+    subcategories: [
+      {
+        title: 'Dresses',
+        items: [
+          'A-Line Dress',
+          'Floaral Dress',
+          'Bodycon Dress',
+          'Cocktail Dress',
+          'Casual Dress',
+        ],
+      },
+      {
+        title: 'Other',
+        items: ['Kurthi Dress', 'Wrap Dress', 'Salwar Dress'],
+      },
+    ],
+  },
+  {
+    category: 'KIDS',
+    subcategories: [
+      {
+        title: 'Boys',
+        items: [
+          'Casual Shirts',
+          'Formal Shirts',
+          'Denim Shirts',
+          'T Shirts',
+          'Hoodies',
+        ],
+      },
+      {
+        title: 'Girls',
+        items: [
+          'A-Line Dress',
+          'Floaral Dress',
+          'Bodycon Dress',
+          'Cocktail Dress',
+          'Casual Dress',
+        ],
+      },
+    ],
+  },
+];
+
+const CategoryHeader = () => {
+  return (
+    <div className="flex justify-between border-b border-gray-500 px-32 p-3">
+      <NavigationMenu>
+        <NavigationMenuList>
+          {categoriesData.map((category, index) => (
+            <NavigationMenuItem key={index}>
+              <NavigationMenuTrigger className="font-black px-4">
+                {category.category}
+              </NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <div className="flex gap-2 p-3">
+                  {category.subcategories.map((subcategory, subIndex) => (
+                    <div key={subIndex}>
+                      <h1 className="font-black px-4">{subcategory.title}</h1>
+                      <ul className="grid gap-1 md:w-[200px] lg:w-[200px]">
+                        {subcategory.items.map((item, itemIndex) => (
+                          <ListItem key={itemIndex} href="/" title={item} />
+                        ))}
+                      </ul>
+                    </div>
+                  ))}
+                </div>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          ))}
+        </NavigationMenuList>
+      </NavigationMenu>
+      <div className="flex space-x-4 items-center justify-items-center content-center">
+        <Search />
+        <Input type="search" placeholder="Search" />
+      </div>
+    </div>
+  );
+};
+
+export default CategoryHeader;
--- vestido/vestido-storefront/layouts/Header.tsx
+++ vestido/vestido-storefront/layouts/Header.tsx
@@ -1,7 +1,39 @@
-const Header =() => {
-    return (
-        <div className="h-64 bg-green border-b-3 mt-10 ">Header</div>
-    )
-}
+import * as React from 'react';
+import { UserRound, Heart, ShoppingCart } from 'lucide-react';
+import CategoryHeader from './CategoryHeader';
+import Image from 'next/image';
+
+const Header = () => (
+  <>
+    <div className="flex justify-between items-center py-4 px-10">
+      <div className="flex justify-between items-center">
+        {/* Logo */}
+        <a href="#">
+          <Image
+            src="/assets/VN-HEADER-2.png"
+            alt="Logo"
+            width="250"
+            height="250"
+          />
+        </a>
+      </div>
+      <div className="flex space-x-4">
+        {/* User Icon */}
+        <a href="/user">
+          <UserRound />
+        </a>
+        {/* Heart Icon */}
+        <a href="/wishlist">
+          <Heart />
+        </a>
+        {/* Shopping Cart Icon */}
+        <a href="/cart">
+          <ShoppingCart />
+        </a>
+      </div>
+    </div>
+    <CategoryHeader />
+  </>
+);
 
 export default Header;
