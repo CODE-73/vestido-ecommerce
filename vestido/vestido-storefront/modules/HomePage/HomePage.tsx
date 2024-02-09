06cf78b14c6a3da0e88a9e1089d8b3e3b601a688
Fathima Sayeeda <sayeeda.61911@gmail.com>
2024-02-09 13:56:06 +0530

    fix: Folder Structure - 2

--- vestido/vestido-storefront/modules/HomePage/HomePage.tsx
+++ vestido/vestido-storefront/modules/HomePage/HomePage.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import { PrimaryCarousel } from './PrimaryCarousel';
-
+import { HorizontalScrollCards } from './HorizontalScrollCards';
 const HomePage: React.FC = () => {
   return (
     <div>
       <PrimaryCarousel />
+      <div className='pt-3'><HorizontalScrollCards/></div>
     </div>
   );
 };
--- vestido/vestido-storefront/pages/index.tsx
+++ vestido/vestido-storefront/pages/index.tsx
@@ -1,11 +1,9 @@
 import HomePage from '../modules/HomePage/HomePage';
-import { HorizontalScrollCards } from '../modules/HomePage/HorizontalScrollCards';
 
 export function Index() {
   return (
     <div>
       <HomePage></HomePage>
-      <div className='pt-3'><HorizontalScrollCards/></div>
     </div>
   );
 }
