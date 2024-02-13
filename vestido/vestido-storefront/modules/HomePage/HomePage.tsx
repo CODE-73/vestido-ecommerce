bd7c715938e84d7a395d163fde2e1c02044ca1ee
Fathima Sayeeda <sayeeda.61911@gmail.com>
2024-02-13 12:51:17 +0530

    feat: Propping Landing Sections with Classnames

--- vestido/vestido-storefront/modules/HomePage/HomePage.tsx
+++ vestido/vestido-storefront/modules/HomePage/HomePage.tsx
@@ -7,13 +7,13 @@ import { FinalSection } from './FinalSection/FinalSection';
 import { WelcomeToStore } from './WelcomeToStore/WelcomeToStore';
 const HomePage: React.FC = () => {
   return (
-    <div >
+    <div className="flex flex-col justify-center">
       <PrimaryCarousel />
-      <div className='pt-3'><HorizontalScrollCards/></div>
-      <div className='pt-16'> <WelcomeToStore/></div>
-      <div className='pt-16'><PopularCollection/></div> 
-      <div className='pt-24'> <SpecialOffer/></div>
-      <div className='pt-16'> <FinalSection/></div>
+      <HorizontalScrollCards className="w-full relative pt-3" />
+      <WelcomeToStore className="pt-10 sm:pt-16" />
+      <PopularCollection className="pt-16" />
+      <SpecialOffer className="pt-16 sm:pt-24 max-w-[100vw] overflow-hidden" />
+      <FinalSection className="pt-16" />
     </div>
   );
 };
