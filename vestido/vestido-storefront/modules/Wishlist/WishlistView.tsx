c61402de8a9149254f8bdc10da107969bfbaf14e
rameez1010 <muhammedrameez1010@gmail.com>
2024-02-27 12:32:13 +0530

    fix: grid change

--- vestido/vestido-storefront/modules/Wishlist/WishlistView.tsx
+++ vestido/vestido-storefront/modules/Wishlist/WishlistView.tsx
@@ -83,107 +83,104 @@ const WishlistView: React.FC = () => {
   const isMobile = useIsMobile();
   const labels = ['Days', 'Hrs', 'Min', 'Sec'];
   return (
-    <div className="">
-      <div className="text-4xl tracking-wide text-[#333333] text-center font-extrabold">
+    <div className="md:px-16">
+      <div className="text-4xl tracking-wide text-[#333333] text-center font-extrabold my-5">
         Wishlist
       </div>
-
-      <div className="">
-        <div className="">
-          {data.map((item, index) => (
-            <div
-              key={index}
-              className="relative flex flex-col items-center group  mb-10 "
-            >
-              <Image
-                className="block group-hover:hidden"
-                src={item.cardImage1}
-                alt="alt text"
-              />
-              <Image
-                className="hidden group-hover:block"
-                src={item.cardImage2}
-                alt="alt text"
-              />
-              {item.salePercent && (
-                <Badge className="absolute left-3 top-3 rounded-none bg-red-600">
-                  SALE {item.salePercent}%
-                </Badge>
-              )}
-              <div className=" absolute bottom-64 sm:bottom-36 bg-white opacity-80 font-extralight text-[#333333] text-lg text-center px-16">
-                <div>Offer Will End Through</div>
-                <div className="flex gap-2 justify-center">
-                  {labels.map((label, index) => (
-                    <div key={index} className="flex flex-col">
-                      <div className="font-extrabold text-2xl">0</div>
-                      <div className="text-xs">{label}</div>
-                    </div>
-                  ))}
-                </div>
-              </div>
-              <div className="flex self-start pt-2 gap-1 ">
-                {Array.from({ length: 5 }).map((_, index) => (
-                  <Star key={index} fill="#ffb503" strokeWidth={0} size={20} />
+      <div className="grid grid-cols-4 gap-10">
+        {data.map((item, index) => (
+          <div
+            key={index}
+            className="relative flex flex-col items-center group  mb-10 "
+          >
+            <Image
+              className="block group-hover:hidden"
+              src={item.cardImage1}
+              alt="alt text"
+            />
+            <Image
+              className="hidden group-hover:block"
+              src={item.cardImage2}
+              alt="alt text"
+            />
+            {item.salePercent && (
+              <Badge className="absolute left-3 top-3 rounded-none bg-red-600">
+                SALE {item.salePercent}%
+              </Badge>
+            )}
+            <div className=" absolute bottom-64 sm:bottom-36 bg-white opacity-80 font-extralight text-[#333333] text-lg text-center px-16">
+              <div>Offer Will End Through</div>
+              <div className="flex gap-2 justify-center">
+                {labels.map((label, index) => (
+                  <div key={index} className="flex flex-col">
+                    <div className="font-extrabold text-2xl">0</div>
+                    <div className="text-xs">{label}</div>
+                  </div>
                 ))}
               </div>
-              <div className="self-start pt-2 text-[#777777] uppercase text-sm font-light">
-                {item.brand}
-              </div>
-              <div className="self-start pt-[#1px] capitalize text-[#333333] text-md font-thin">
-                {item.name}
-              </div>
-              {isMobile ? (
-                <>
-                  <div className="flex text-xl justify-between w-full pt-3">
+            </div>
+            <div className="flex self-start pt-2 gap-1 ">
+              {Array.from({ length: 5 }).map((_, index) => (
+                <Star key={index} fill="#ffb503" strokeWidth={0} size={20} />
+              ))}
+            </div>
+            <div className="self-start pt-2 text-[#777777] uppercase text-sm font-light">
+              {item.brand}
+            </div>
+            <div className="self-start pt-[#1px] capitalize text-[#333333] text-md font-thin">
+              {item.name}
+            </div>
+            {isMobile ? (
+              <>
+                <div className="flex text-xl justify-between w-full pt-3">
+                  <div className="line-through">{item.price}</div>
+                  <div className="text-red-700 font-bold">
+                    {item.offerPrice}
+                  </div>
+                </div>
+                <div className={`p-2 bg-[#48CAB2] w-full`}>
+                  <Button className="bg-[#48CAB2] w-full flex gap-3 text-lg mb-1 text-white p-2 font-bold">
+                    <ShoppingBag color="#fff" />
+                    <div> Add to Cart</div>
+                  </Button>
+                </div>
+              </>
+            ) : (
+              <div className="self-start mt-2">
+                <div
+                  className="flex gap-1 relative items-center"
+                  onMouseEnter={() => setIsHovering(true)}
+                  onMouseLeave={() => setIsHovering(false)}
+                >
+                  <div
+                    className={`p-2 bg-[#48CAB2] ${
+                      isHovering ? 'w-20' : 'w-auto'
+                    }`}
+                    style={{ minWidth: 'min-content' }}
+                  >
+                    <ShoppingBag color="#fff" />
+                    {isHovering && (
+                      <button className=" absolute top-1 right-0 bg-[#48CAB2] text-white p-2 font-bold">
+                        Add to Cart
+                      </button>
+                    )}
+                  </div>
+                  <div className="flex flex-col">
                     <div className="line-through">{item.price}</div>
                     <div className="text-red-700 font-bold">
                       {item.offerPrice}
                     </div>
                   </div>
-                  <div className={`p-2 bg-[#48CAB2] w-full`}>
-                    <Button className="bg-[#48CAB2] w-full flex gap-3 text-lg mb-1 text-white p-2 font-bold">
-                      <ShoppingBag color="#fff" />
-                      <div> Add to Cart</div>
-                    </Button>
-                  </div>
-                </>
-              ) : (
-                <div className="self-start mt-2">
-                  <div
-                    className="flex gap-1 relative items-center"
-                    onMouseEnter={() => setIsHovering(true)}
-                    onMouseLeave={() => setIsHovering(false)}
-                  >
-                    <div
-                      className={`p-2 bg-[#48CAB2] ${
-                        isHovering ? 'w-20' : 'w-auto'
-                      }`}
-                      style={{ minWidth: 'min-content' }}
-                    >
-                      <ShoppingBag color="#fff" />
-                      {isHovering && (
-                        <button className=" absolute top-1 right-0 bg-[#48CAB2] text-white p-2 font-bold">
-                          Add to Cart
-                        </button>
-                      )}
-                    </div>
-                    <div className="flex flex-col">
-                      <div className="line-through">{item.price}</div>
-                      <div className="text-red-700 font-bold">
-                        {item.offerPrice}
-                      </div>
-                    </div>
-                  </div>
                 </div>
-              )}
-              <div className="sm:hidden flex flex-row justify-start sm:group-hover:flex sm:flex-col gap-3 sm:absolute top-3 right-3 pt-2 sm:pt-0">
-                <AddToWishListButton />
-                <AddToCompareButton />
-                <QuickViewButton />
               </div>
+            )}
+            <div className="sm:hidden flex flex-row justify-start sm:group-hover:flex sm:flex-col gap-3 sm:absolute top-3 right-3 pt-2 sm:pt-0">
+              <AddToWishListButton />
+              <AddToCompareButton />
+              <QuickViewButton />
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
     </div>
   );
