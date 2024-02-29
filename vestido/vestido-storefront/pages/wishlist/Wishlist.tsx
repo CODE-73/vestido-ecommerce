a2353553f06a3e6c565157a6bcd5acdea9f3fe1c
Muhammed Rameez <109953720+rameez1010@users.noreply.github.com>
2024-02-29 11:01:33 +0530

    feat: Wishlist Page (#10)
    * feat: Wishlist Page
    
    * fix: grid change
    
    ---------
    
    Co-authored-by: Mohammed Fabinsha K <75523681+mohammedfabinsha@users.noreply.github.com>

---
+++ vestido/vestido-storefront/pages/wishlist/Wishlist.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+import WishlistView from 'vestido/vestido-storefront/modules/Wishlist/WishlistView';
+
+const Wishlist = () => {
+  return <WishlistView />;
+};
+
+export default Wishlist;
