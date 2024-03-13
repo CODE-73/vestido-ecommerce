222be05e2a5941f941d487483ece1a36067936e7
Muhammed Rameez <109953720+rameez1010@users.noreply.github.com>
2024-03-13 11:23:51 +0530

    feat: Title Changed (#16)
    * feat: Title Changed
    
    * fix: IOS Video Issue
    
    ---------
    
    Co-authored-by: Mohammed Fabinsha K <75523681+mohammedfabinsha@users.noreply.github.com>

--- vestido/vestido-landing/app/background-video.tsx
+++ vestido/vestido-landing/app/background-video.tsx
@@ -3,7 +3,13 @@
 const BackgroundVideo = () => {
   return (
     <div className="h-full w-full overflow-hidden absolute opacity-90 -z-10">
-      <video className="w-full h-full object-cover" autoPlay loop muted>
+      <video
+        className="w-full h-full object-cover"
+        autoPlay
+        loop
+        muted
+        playsInline
+      >
         <source src="/videos/video.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
