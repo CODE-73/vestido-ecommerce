c32fa3e4e40d906cb71cbd14b8b48fad13ade8ae
rameez1010 <muhammedrameez1010@gmail.com>
2024-03-13 08:44:21 +0530

    feat: Title Changed

--- vestido/vestido-dashboard/pages/_app.tsx
+++ vestido/vestido-dashboard/pages/_app.tsx
@@ -6,7 +6,7 @@ function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to vestido-dashboard!</title>
+        <title>VestidoNation</title>
       </Head>
       <main className="app">
         <Component {...pageProps} />
