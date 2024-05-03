12076c58a6804c21dbd8ec6490c061f06abd9882
Fathima Sayeeda <sayeeda.61911@gmail.com>
2024-05-03 16:05:01 +0530

    fix: Add useAuth to getCart Service

--- libs/items/src/swr/cart/get-cart/service.ts
+++ libs/items/src/swr/cart/get-cart/service.ts
@@ -1,12 +1,28 @@
 import { CartResponse } from './types';
 
-export async function getCart(): Promise<CartResponse> {
-  const r = await fetch('/api/cart');
-  if (!r.ok) {
+import axios from 'axios'; // Import Axios
+
+export async function getCart(
+  authHeaders: Record<string, string>
+): Promise<CartResponse> {
+  try {
+    const r = await await axios.get('/api/cart', {
+      headers: {
+        ...authHeaders,
+      },
+    });
+    console.log('the cart is below', r.data);
+    return r.data as CartResponse;
+  } catch (error) {
+    console.error('Error Fetching Cart:', error);
     throw new Error('Error Fetching Cart');
   }
-
-  const data = await r.json();
-  console.log('the cart is below', data);
-  return data as CartResponse;
 }
+//   if (!r.ok) {
+//     throw new Error('Error Fetching Cart');
+//   }
+
+//   const data = await r.json();
+//   console.log('the cart is below', data);
+//   return data as CartResponse;
+// }
