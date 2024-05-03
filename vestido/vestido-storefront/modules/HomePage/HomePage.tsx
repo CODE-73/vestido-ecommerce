3479019ea4f40f351af7414a3ebeeafaa2eb638e
Fathima Sayeeda <sayeeda.61911@gmail.com>
2024-05-03 15:03:24 +0530

    fix: Home and Carousel minor fixes

--- vestido/vestido-storefront/modules/HomePage/HomePage.tsx
+++ vestido/vestido-storefront/modules/HomePage/HomePage.tsx
@@ -1,19 +1,18 @@
 import * as React from 'react';
 import { PrimaryCarousel } from './PrimaryCarousel/PrimaryCarousel';
-import { HorizontalScrollCards } from './HorizontalScroll/HorizontalScrollCards';
+// import { SpecialOffer } from './SpecialOffer/SpecialOffer';
+import { CategoryCards } from './CategorySection/CategoryCards';
+import { TopProducts } from './TopProducts/TopProducts';
 import { PopularCollection } from './Popular/PopularCollection';
-import { SpecialOffer } from './SpecialOffer/SpecialOffer';
-import { FinalSection } from './FinalSection/FinalSection';
-import { WelcomeToStore } from './WelcomeToStore/WelcomeToStore';
+import { HorizontalScrollCards } from './HorizontalScroll/HorizontalScrollCards';
 const HomePage: React.FC = () => {
   return (
     <div className="flex flex-col justify-center">
       <PrimaryCarousel />
-      <HorizontalScrollCards className="w-full relative pt-3 px-1 sm:px-0" />
-      <WelcomeToStore className="pt-10 sm:pt-16 px-1 sm:px-0" />
+      <CategoryCards className="pt-4" />
+      <TopProducts className="pt-16 sm:pt-24 max-w-[100vw] overflow-hidden px-4 sm:px-0" />
       <PopularCollection className="pt-16 px-1 sm:px-0" />
-      <SpecialOffer className="pt-16 sm:pt-24 max-w-[100vw] overflow-hidden px-4 sm:px-0" />
-      <FinalSection className="pt-16 px-1 sm:px-0" />
+      <HorizontalScrollCards className="w-full relative pt-24 px-1 sm:px-0" />
     </div>
   );
 };
