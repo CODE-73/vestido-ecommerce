f1b46397ab337bee742153daacb2dadbf0cd658a
Muhammed Rameez <109953720+rameez1010@users.noreply.github.com>
2024-05-07 15:19:10 +0530

    feat: Added Back To Top Button (#44)

--- vestido/vestido-storefront/modules/HomePage/HomePage.tsx
+++ vestido/vestido-storefront/modules/HomePage/HomePage.tsx
@@ -1,11 +1,46 @@
 import * as React from 'react';
 import { PrimaryCarousel } from './PrimaryCarousel/PrimaryCarousel';
-// import { SpecialOffer } from './SpecialOffer/SpecialOffer';
 import { CategoryCards } from './CategorySection/CategoryCards';
 import { TopProducts } from './TopProducts/TopProducts';
 import { PopularCollection } from './Popular/PopularCollection';
 import { HorizontalScrollCards } from './HorizontalScroll/HorizontalScrollCards';
+import { ChevronUp } from 'lucide-react';
+import useIsMobile from '../../hooks/useIsMobile';
+
+const BackToTopButton: React.FC<{ onClick: () => void }> = ({ onClick }) => (
+  <button
+    className="fixed bottom-8 right-8 bg-neutral-800 hover:bg-[#48CAB2] text-white px-4 py-4 shadow transition-colors duration-200"
+    onClick={onClick}
+  >
+    <ChevronUp />
+  </button>
+);
+
 const HomePage: React.FC = () => {
+  const [showBackToTop, setShowBackToTop] = React.useState(false);
+  const isMobile = useIsMobile();
+
+  React.useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop =
+        window.pageYOffset || document.documentElement.scrollTop;
+      setShowBackToTop(scrollTop > 100);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
   return (
     <div className="flex flex-col justify-center">
       <PrimaryCarousel />
@@ -13,6 +48,8 @@ const HomePage: React.FC = () => {
       <TopProducts className="pt-16 sm:pt-24 max-w-[100vw] overflow-hidden px-4 sm:px-0" />
       <PopularCollection className="pt-16 px-1 sm:px-0" />
       <HorizontalScrollCards className="w-full relative pt-24 px-1 sm:px-0" />
+
+      {!isMobile && showBackToTop && <BackToTopButton onClick={scrollToTop} />}
     </div>
   );
 };
