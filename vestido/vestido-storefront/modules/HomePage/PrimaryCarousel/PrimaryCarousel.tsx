e5e4fba7a6bc92f8c839a623e4c59e1ed2c9144a
Fathima Sayeeda <sayeeda.61911@gmail.com>
2024-05-11 11:56:15 +0530

    fix: Carousel Dot Button

--- vestido/vestido-storefront/modules/HomePage/PrimaryCarousel/PrimaryCarousel.tsx
+++ vestido/vestido-storefront/modules/HomePage/PrimaryCarousel/PrimaryCarousel.tsx
@@ -3,12 +3,17 @@ import Autoplay from 'embla-carousel-autoplay';
 import image1 from '../../../assets/carousel/slide-1.jpg';
 import image2 from '../../../assets/carousel/slide-2.jpg';
 import image3 from '../../../assets/carousel/slide-3.jpg';
-import { Carousel, CarouselContent } from 'libs/shadcn-ui/src/ui/carousel';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselDots,
+} from 'libs/shadcn-ui/src/ui/carousel';
 import PrimaryCarouselItem, {
   PrimaryCarouselItemData,
 } from './PrimaryCarouselItem';
+import clsx from 'clsx';
 
-const slides: PrimaryCarouselItemData[] = [
+const carouselSlides: PrimaryCarouselItemData[] = [
   {
     backgroundImage: image1,
     mainTitle: 'find your new favourite clothing',
@@ -44,25 +49,39 @@ const slides: PrimaryCarouselItemData[] = [
     textPosition: 'right-6',
   },
 ];
-export function PrimaryCarousel() {
+
+export const PrimaryCarousel: React.FC = () => {
   const plugin = React.useRef(
-    Autoplay({ delay: 2000, stopOnInteraction: false, stopOnMouseEnter: true })
+    Autoplay({ delay: 4000, stopOnInteraction: false, stopOnMouseEnter: true })
   );
 
   return (
     <div className="grid grid-cols-8 gap-12">
       <Carousel
         plugins={[plugin.current]}
-        className="col-span-8 sm:col-start-3 sm:col-span-6 sm:p-4 pr-0"
+        className="col-span-8 sm:col-start-3 sm:col-span-6 sm:p-4 pr-0 relative"
         onMouseEnter={plugin.current.stop}
         onMouseLeave={plugin.current.reset}
       >
         <CarouselContent>
-          {slides.map((slide, index) => (
+          {carouselSlides.map((slide, index) => (
             <PrimaryCarouselItem data={slide} key={index} />
           ))}
         </CarouselContent>
+        <div className="z-50 absolute bottom-10 left-[45%]">
+          <CarouselDots>
+            {({ scrollSnap, onClick, selectedIndex, index }) => (
+              <button
+                className={clsx('rounded-none h-1 w-3 bg-white', {
+                  'bg-[#333] w-12': selectedIndex === index,
+                })}
+                key={scrollSnap}
+                onClick={() => onClick(index)}
+              ></button>
+            )}
+          </CarouselDots>
+        </div>
       </Carousel>
     </div>
   );
-}
+};
