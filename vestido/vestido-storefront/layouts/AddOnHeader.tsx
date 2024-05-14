96e9091cbf0dfef5a26f008c9afb81a27b50fc2a
Fathima Sayeeda <sayeeda.61911@gmail.com>
2024-05-14 16:37:45 +0530

    fix: Carousel

--- vestido/vestido-storefront/layouts/AddOnHeader.tsx
+++ vestido/vestido-storefront/layouts/AddOnHeader.tsx
@@ -1,17 +1,60 @@
+import * as React from 'react';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from '@vestido-ecommerce/shadcn-ui/carousel';
+import Autoplay from 'embla-carousel-autoplay';
+
 const AddOnHeader = () => {
+  const messages = [
+    {
+      title: 'FREE 2-DAYS',
+      description: 'standard shipping on orders $255',
+    },
+    {
+      title: 'Your second message',
+      description: 'Your second description',
+    },
+  ];
+
+  const plugin = React.useRef(
+    Autoplay({ delay: 4000, stopOnInteraction: false, stopOnMouseEnter: true })
+  );
+
   return (
-    <div className="flex flex-row text-[#333] text-sm py-1 pt-2 justify-between">
-      <div className="flex flex-row text-gray-500">
-        {' '}
+    <div className="grid grid-cols-1 sm:grid-cols-3 text-[#333] text-sm py-1 pt-2 justify-between">
+      <div className="flex text-gray-500">
         <div>Call Us:</div>
         <div className="font-extrabold">1–234–5678901</div>
       </div>
-      <div className="flex text-gray-500">
-        {' '}
-        <div className="text-[#333] text-sm font-bold">FREE 2-DAYS &nbsp;</div>
-        <div> standard shipping on orders $255</div>
-      </div>
-
+      <Carousel
+        opts={{
+          align: 'start',
+          loop: true,
+        }}
+        plugins={[plugin.current]}
+        onMouseEnter={plugin.current.stop}
+        onMouseLeave={plugin.current.reset}
+        className="justify-self-center"
+      >
+        <CarouselContent>
+          {' '}
+          {messages.map((message, index) => (
+            <div key={index} className="flex-shrink-0 w-full">
+              <CarouselItem>
+                {' '}
+                <div className="flex">
+                  <div className="text-[#333] text-sm font-bold">
+                    {message.title}&nbsp;
+                  </div>
+                  <div>{message.description}</div>
+                </div>
+              </CarouselItem>
+            </div>
+          ))}
+        </CarouselContent>
+      </Carousel>
       <div className="flex gap-3 text-gray-500 justify-end pb-2">
         <a
           href="/wishlist"
