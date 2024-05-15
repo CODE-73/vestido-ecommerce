0da075dccca8ffcd6648b166bdc9c5a24e5e07a2
Fathima Sayeeda <sayeeda.61911@gmail.com>
2024-05-15 18:46:25 +0530

    fix: Dashboard Category Form Bugs

--- vestido/vestido-dashboard/pages/categories/add-new.tsx
+++
@@ -1,6 +0,0 @@
-import { NextPage } from 'next';
-import CategoryForm from '../../modules/categories/CategoryForm';
-
-const AddCategoryPage: NextPage = () => <CategoryForm isNew={true} />;
-
-export default AddCategoryPage;
