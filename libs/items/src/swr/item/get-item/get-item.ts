42987b8b1b95449a25ac455762d506d674d9fa97
Fathima Sayeeda <sayeeda.61911@gmail.com>
2024-05-15 18:38:53 +0530

    fix: Dashboard Product Form Bugs

--- libs/items/src/swr/item/get-item/get-item.ts
+++ libs/items/src/swr/item/get-item/get-item.ts
@@ -3,12 +3,14 @@ import { ItemDetailsResponse } from '../../../../../items/src/services/get-item/
 import { getItemDetails } from './service';
 import { ItemDetailsSWRKeys } from '../keys';
 
-export function useItem(itemId: string) {
-  const key = [ItemDetailsSWRKeys.ITEM, ItemDetailsSWRKeys.DETAILS, itemId];
+export function useItem(itemId?: string | null) {
+  const key = itemId
+    ? [ItemDetailsSWRKeys.ITEM, ItemDetailsSWRKeys.DETAILS, itemId]
+    : null;
 
   return useSWRImmutable<ItemDetailsResponse, Error>(
     key,
-    () => getItemDetails(itemId),
+    () => getItemDetails(itemId as string),
     {
       keepPreviousData: true,
     }
--- vestido/vestido-dashboard/pages/products/add-new.tsx
+++
@@ -1,6 +0,0 @@
-import { NextPage } from 'next';
-import ProductForm from '../../modules/products/ProductForm';
-
-const AddProductPage: NextPage = () => <ProductForm isNew={true} />;
-
-export default AddProductPage;
