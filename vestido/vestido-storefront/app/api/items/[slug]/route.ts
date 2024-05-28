222ed55ccfc34a05c10bfddcc9d8c3e9884dc84b
Fathima Sayeeda <sayeeda.61911@gmail.com>
2024-05-28 11:28:23 +0530

    fix: Storefront API

--- vestido/vestido-storefront/app/api/items/[slug]/route.ts
+++ vestido/vestido-storefront/app/api/items/[slug]/route.ts
@@ -1,5 +1,4 @@
-import { itemDetails, updateItem, deleteItem } from '@vestido-ecommerce/items';
-import { ZodError } from 'zod';
+import { itemDetails } from '@vestido-ecommerce/items';
 
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
 
@@ -19,67 +18,6 @@ export async function GET(
     console.error(e);
     return new Response(JSON.stringify(e), {
       status: 500,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-  }
-}
-
-export async function PUT(
-  request: Request,
-  { params }: { params: { slug: string } }
-) {
-  const body = await request.json();
-
-  try {
-    const r = await updateItem(params.slug, body);
-
-    return new Response(JSON.stringify(r), {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-  } catch (e) {
-    if (e instanceof ZodError) {
-      return new Response(JSON.stringify(e), {
-        status: 400,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-    } else {
-      console.error('Unexpected Error', e);
-      return new Response(
-        JSON.stringify({
-          message: 'Unknown Error',
-        }),
-        {
-          status: 500,
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-    }
-  }
-}
-export async function DELETE(
-  request: Request,
-  { params }: { params: { slug: string } }
-) {
-  try {
-    await deleteItem(params.slug);
-
-    return new Response(JSON.stringify(params.slug), {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-  } catch (e) {
-    console.error(e);
-    return new Response(JSON.stringify(e), {
-      status: 400,
       headers: {
         'Content-Type': 'application/json',
       },
--- vestido/vestido-storefront/app/api/items/route.ts
+++ vestido/vestido-storefront/app/api/items/route.ts
@@ -1,6 +1,4 @@
-import { createItem, listItem } from '@vestido-ecommerce/items';
-import { ZodError } from 'zod';
-import { verifyAuth } from '../verify-auth';
+import { listItem } from '@vestido-ecommerce/items';
 
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
 
@@ -21,47 +19,5 @@ export async function GET(request: Request) {
         'Content-Type': 'application/json',
       },
     });
-  }
-}
-
-export async function POST(request: Request) {
-  try {
-    const body = await request.json();
-    console.log('body is', body);
-
-    // Call the createItem function with the validated request body
-    const newItem = await createItem(body);
-
-    const r = {
-      data: newItem,
-    };
-
-    return new Response(JSON.stringify(r), {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-  } catch (e) {
-    if (e instanceof ZodError) {
-      return new Response(JSON.stringify(e), {
-        status: 400,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-    } else {
-      console.error('Unexpected Error', e);
-      return new Response(
-        JSON.stringify({
-          message: 'Unknown Error',
-        }),
-        {
-          status: 500,
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-    }
   }
 }
