8af140cafb66318a5988ef73c817080416c28711
Fathima Sayeeda <sayeeda.61911@gmail.com>
2024-06-25 10:27:10 +0530

    fix: createOrderSchema

--- libs/orders/src/services/orders/create-order/service.ts
+++ libs/orders/src/services/orders/create-order/service.ts
@@ -5,12 +5,24 @@ export async function createOrder(data: CreateOrderSchemaType) {
   const prisma = new PrismaClient();
 
   // validate zod here
-  const validatedData = CreateOrderSchema.parse(data);
+  const { addressId, customerId, ...validatedData } =
+    CreateOrderSchema.parse(data);
   // pass to prisma next
 
   const newOrder = await prisma.order.create({
     data: {
       ...validatedData,
+      status: 'PENDING',
+      customer: {
+        connect: {
+          id: customerId,
+        },
+      },
+      shippingAddress: {
+        connect: {
+          id: addressId,
+        },
+      },
       orderItems: {
         createMany: {
           data: validatedData.orderItems,
--- libs/orders/src/services/orders/create-order/zod.ts
+++ libs/orders/src/services/orders/create-order/zod.ts
@@ -1,20 +1,16 @@
 import { z } from 'zod';
-import { OrderStatus } from '@prisma/client';
 
 export const OrderItemSchema = z.object({
+  itemId: z.string().uuid(),
   price: z.coerce.number(),
   qty: z.number().int(),
-  orderId: z.string().uuid(),
 });
 
 export const CreateOrderSchema = z.object({
   dateTime: z.string().datetime(),
   totalPrice: z.coerce.number(),
   itemsCount: z.number().int(),
-  orderStatus: z
-    .nativeEnum(OrderStatus)
-    .default('PENDING' satisfies OrderStatus),
-  shippingAddress: z.string().uuid(),
+  addressId: z.string().uuid(),
   customerId: z.string().uuid(),
   orderItems: z.array(OrderItemSchema),
 });
