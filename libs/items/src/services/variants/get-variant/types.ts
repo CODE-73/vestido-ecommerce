d300e52e2a4320e64a7e7536eff08c1eab778a38
Fathima Sayeeda <sayeeda.61911@gmail.com>
2024-07-04 16:03:44 +0530

    fix:Build Error Dashboard

--- libs/items/src/services/variants/get-variant/types.ts
+++ libs/items/src/services/variants/get-variant/types.ts
@@ -1,9 +1,7 @@
 import { ItemVariant, VariantAttributeValue } from '@prisma/client';
-import { ImageSchemaType } from '@vestido-ecommerce/utils';
 
 export type VariantDetailsResponse = {
   data: ItemVariant & {
     attributeValues: VariantAttributeValue[];
-    images: ImageSchemaType[];
   };
 };
