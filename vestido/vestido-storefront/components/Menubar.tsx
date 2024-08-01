9dd3d47e420f23e6532561c99f3d4b394af1c332
Fathima Sayeeda <sayeeda.61911@gmail.com>
2024-08-01 18:47:59 +0530

    fix: MInor changes in Menubar

--- vestido/vestido-storefront/components/Menubar.tsx
+++ vestido/vestido-storefront/components/Menubar.tsx
@@ -1,16 +1,14 @@
 import * as React from 'react';
 import Link from 'next/link';
+
 import { Gender, useCategories } from '@vestido-ecommerce/items';
-
-
 import {
   Menubar,
   MenubarContent,
   MenubarItem,
   MenubarMenu,
   MenubarTrigger,
-} from "@vestido-ecommerce/shadcn-ui/menubar"
-
+} from '@vestido-ecommerce/shadcn-ui/menubar';
 
 type ListItemProps = {
   href: string;
@@ -49,103 +47,31 @@ const HeaderMenubar: React.FC<NavMenuProps> = ({ isFixed }) => {
   };
 
   return (
-    <Menubar className='bg-transparent border-none'>
-     <MenubarMenu>
-     
-          <MenubarTrigger
-            className={`font-semibold h-6 bg-transparent hover:bg-transparent data-active:bg-transparent data-[state=open]:bg-transparent hover:text-green-300 data-[state=open]:text-green-300 rounded-none mx-3 focus:bg-transparent ${
-              isFixed ? '' : 'text-white focus:text-white '
-            }`}
-          >
-            MEN
-          </MenubarTrigger>
-
-          <MenubarContent className="w-[100px] ">
-            {mainCategories
-              ?.filter((category) => category.gender.includes('MEN'))
-              .map((category, index) => (
-                <div key={index}>
-                  <MenubarItem className=" text-stone-500 capitalize hover:text-[#48cab2] px-2 cursor-pointer">
-                    <Link href={`/${category.id}`}>{category.name}</Link>
-                  </MenubarItem>
-
-                  <ul className="text-stone-500 capitalize py-3 md:w-[200px] lg:w-[200px]">
-                    {getSubcategories(category.id, ['MEN'])?.map(
-                      (subcategory, subIndex) => (
-                        <MenubarItem key={subIndex} className="hover:text-green-300">
-                          <ListItem
-                            href={`/${subcategory.id}`}
-                            title={subcategory.name}
-                          />
-                        </MenubarItem>
-                      ),
-                    )}
-                  </ul>
-                </div>
-              ))}
-          </MenubarContent> </MenubarMenu>
-       
-          <MenubarMenu>
-          <MenubarTrigger
-            className={`font-semibold h-6 bg-transparent hover:bg-transparent data-active:bg-transparent data-[state=open]:bg-transparent data-[state=open]:text-green-300 hover:text-green-300 rounded-none mx-3 focus:bg-transparent ${
-              isFixed ? '' : 'text-white focus:text-white '
-            }`}
-          >
-            WOMEN
-          </MenubarTrigger>
-
-          <MenubarContent className="w-[100px] ">
-            {mainCategories
-              ?.filter((category) => category.gender.includes('WOMEN'))
-              .map((category, index) => (
-                <div key={index}>
-                  <MenubarItem className=" text-stone-500 capitalize hover:text-[#48cab2] px-2 cursor-pointer">
-                    <Link href={`/${category.id}`}>{category.name}</Link>
-                  </MenubarItem>
-
-                  <ul className="text-stone-500 capitalize py-3 md:w-[200px] lg:w-[200px]">
-                    {getSubcategories(category.id, ['WOMEN'])?.map(
-                      (subcategory, subIndex) => (
-                        <MenubarItem key={subIndex} className="hover:text-green-300">
-                          <ListItem
-                            href={`/${subcategory.id}`}
-                            title={subcategory.name}
-                          />
-                        </MenubarItem>
-                      ),
-                    )}
-                  </ul>
-                </div>
-              ))}
-          </MenubarContent> </MenubarMenu>
-       
-          <MenubarMenu>
-          <MenubarTrigger
-            className={`font-semibold h-6 bg-transparent hover:bg-transparent data-active:bg-transparent data-[state=open]:bg-transparent data-[state=open]:text-green-300 hover:text-green-300 rounded-none mx-3 focus:bg-transparent ${
-              isFixed ? '' : 'text-white focus:text-white '
-            }`}
-          >
-            UNISEX
-          </MenubarTrigger>
-
-          <MenubarContent className=" w-[100px] ">
-
-            {mainCategories
-              ?.filter(
-                (category) =>
-                  category.gender.includes('MEN') &&
-                  category.gender.includes('WOMEN'),
-              )
-              .map((category, index) => (
-                <div key={index}>
+    <Menubar className="bg-transparent border-none">
+      <MenubarMenu>
+        <MenubarTrigger
+          className={`font-semibold h-6 bg-transparent hover:bg-transparent data-active:bg-transparent data-[state=open]:bg-transparent hover:text-green-300 data-[state=open]:text-green-300 rounded-none mx-3 focus:bg-transparent ${
+            isFixed ? '' : 'text-white focus:text-white '
+          }`}
+        >
+          MEN
+        </MenubarTrigger>
+        <MenubarContent className="w-[100px] ">
+          {mainCategories
+            ?.filter((category) => category.gender.includes('MEN'))
+            .map((category, index) => (
+              <div key={index}>
                 <MenubarItem className=" text-stone-500 capitalize hover:text-[#48cab2] px-2 cursor-pointer">
                   <Link href={`/${category.id}`}>{category.name}</Link>
                 </MenubarItem>
 
                 <ul className="text-stone-500 capitalize py-3 md:w-[200px] lg:w-[200px]">
-                  {getSubcategories(category.id, ['MEN', 'WOMEN'])?.map(
+                  {getSubcategories(category.id, ['MEN'])?.map(
                     (subcategory, subIndex) => (
-                      <MenubarItem key={subIndex} className="hover:text-green-300">
+                      <MenubarItem
+                        key={subIndex}
+                        className="hover:text-green-300"
+                      >
                         <ListItem
                           href={`/${subcategory.id}`}
                           title={subcategory.name}
@@ -155,11 +81,89 @@ const HeaderMenubar: React.FC<NavMenuProps> = ({ isFixed }) => {
                   )}
                 </ul>
               </div>
-              ))}
-          </MenubarContent>
-        
-        </MenubarMenu>
-      </Menubar>
+            ))}
+        </MenubarContent>
+      </MenubarMenu>
+
+      <MenubarMenu>
+        <MenubarTrigger
+          className={`font-semibold h-6 bg-transparent hover:bg-transparent data-active:bg-transparent data-[state=open]:bg-transparent data-[state=open]:text-green-300 hover:text-green-300 rounded-none mx-3 focus:bg-transparent ${
+            isFixed ? '' : 'text-white focus:text-white '
+          }`}
+        >
+          WOMEN
+        </MenubarTrigger>
+        <MenubarContent className="w-[100px] ">
+          {mainCategories
+            ?.filter((category) => category.gender.includes('WOMEN'))
+            .map((category, index) => (
+              <div key={index}>
+                <MenubarItem className=" text-stone-500 capitalize hover:text-[#48cab2] px-2 cursor-pointer">
+                  <Link href={`/${category.id}`}>{category.name}</Link>
+                </MenubarItem>
+
+                <ul className="text-stone-500 capitalize py-3 md:w-[200px] lg:w-[200px]">
+                  {getSubcategories(category.id, ['WOMEN'])?.map(
+                    (subcategory, subIndex) => (
+                      <MenubarItem
+                        key={subIndex}
+                        className="hover:text-green-300"
+                      >
+                        <ListItem
+                          href={`/${subcategory.id}`}
+                          title={subcategory.name}
+                        />
+                      </MenubarItem>
+                    ),
+                  )}
+                </ul>
+              </div>
+            ))}
+        </MenubarContent>{' '}
+      </MenubarMenu>
+
+      <MenubarMenu>
+        <MenubarTrigger
+          className={`font-semibold h-6 bg-transparent hover:bg-transparent data-active:bg-transparent data-[state=open]:bg-transparent data-[state=open]:text-green-300 hover:text-green-300 rounded-none mx-3 focus:bg-transparent ${
+            isFixed ? '' : 'text-white focus:text-white '
+          }`}
+        >
+          UNISEX
+        </MenubarTrigger>
+
+        <MenubarContent className=" w-[100px] ">
+          {mainCategories
+            ?.filter(
+              (category) =>
+                category.gender.includes('MEN') &&
+                category.gender.includes('WOMEN'),
+            )
+            .map((category, index) => (
+              <div key={index}>
+                <MenubarItem className=" text-stone-500 capitalize hover:text-[#48cab2] px-2 cursor-pointer">
+                  <Link href={`/${category.id}`}>{category.name}</Link>
+                </MenubarItem>
+
+                <ul className="text-stone-500 capitalize py-3 md:w-[200px] lg:w-[200px]">
+                  {getSubcategories(category.id, ['MEN', 'WOMEN'])?.map(
+                    (subcategory, subIndex) => (
+                      <MenubarItem
+                        key={subIndex}
+                        className="hover:text-green-300"
+                      >
+                        <ListItem
+                          href={`/${subcategory.id}`}
+                          title={subcategory.name}
+                        />
+                      </MenubarItem>
+                    ),
+                  )}
+                </ul>
+              </div>
+            ))}
+        </MenubarContent>
+      </MenubarMenu>
+    </Menubar>
   );
 };
 
