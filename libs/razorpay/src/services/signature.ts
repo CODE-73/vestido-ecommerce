3da12392f332f71432d2f7176346bc833d2bdc5b
Fahim Ali Zain <fahimalizain@gmail.com>
2024-08-12 20:23:32 +0530

    chore: Update .env

--- libs/razorpay/src/services/signature.ts
+++ libs/razorpay/src/services/signature.ts
@@ -1,19 +1,22 @@
 import * as CryptoJS from 'crypto-js';
 
-const razorpaySecret = process.env['RAZORPAY_KEY_SECRET'] as string; // Your Razorpay secret key
+const RAZORPAY_KEY_SECRET = process.env['RAZORPAY_KEY_SECRET'] as string;
+const RAZORPAY_WEBHOOK_SECRET = process.env[
+  'RAZORPAY_WEBHOOK_SECRET'
+] as string;
+
 export function generatePaymentSignature(
   order_id: string,
   payment_RP_id: string,
 ) {
   return CryptoJS.HmacSHA256(
     `${order_id}|${payment_RP_id}`,
-    razorpaySecret,
+    RAZORPAY_KEY_SECRET,
   ).toString(CryptoJS.enc.Hex);
 }
 
-const webhookSecret = '6d2Lv76AugGojJGt1Wa7MEgFZmSZaq5z';
 export function generateWebhookSignature(dataString: string) {
-  return CryptoJS.HmacSHA256(dataString, webhookSecret).toString(
+  return CryptoJS.HmacSHA256(dataString, RAZORPAY_WEBHOOK_SECRET).toString(
     CryptoJS.enc.Hex,
   );
 }
