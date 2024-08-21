ba355613a0f37279a12191c36ec3fd9b518c22a8
Fahim Ali Zain <fahimalizain@gmail.com>
2024-08-21 14:10:43 +0530

    feat: Fast2SMS Integration

--- libs/auth/src/services/sendOtp/service.ts
+++ libs/auth/src/services/sendOtp/service.ts
@@ -1,13 +1,26 @@
 import { generateOTP } from '@vestido-ecommerce/caching';
+import { sendSMS, SMSSenderID, SMSTemplate } from '@vestido-ecommerce/fast2sms';
 
 import { verifyUserExist } from '../verifyUser';
 import { SendOtpSchema, SendOtpSchemaType } from './zod';
+
+const IS_DEVELOPMENT = process.env['NODE_ENV'] === 'development';
 
 export async function sendOTP(data: SendOtpSchemaType) {
   const { mobile } = SendOtpSchema.parse(data);
 
   const otp = await generateOTP(mobile);
-  console.info('OTP:', mobile, otp);
+
+  if (!IS_DEVELOPMENT) {
+    await sendSMS({
+      senderId: SMSSenderID.BVSTID,
+      template: SMSTemplate.OTP_SMS,
+      variables: [otp],
+      recipients: [mobile],
+    });
+  } else {
+    console.info('OTP:', mobile, otp);
+  }
 
   const user = await verifyUserExist({ mobile: mobile });
   const userExists = !!user;
