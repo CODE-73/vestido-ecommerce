d9f851808b7defee056a729092baadfa215efe34
Fathima Sayeeda <sayeeda.61911@gmail.com>
2024-09-07 08:05:28 +0530

    fix: Update dashboard/api/attribute routes

--- vestido/vestido-dashboard/app/api/attributes/[slug]/route.ts
+++ vestido/vestido-dashboard/app/api/attributes/[slug]/route.ts
@@ -1,120 +1,24 @@
-import { ZodError } from 'zod';
-
 import {
   attributeDetails,
   deleteAttribute,
   updateAttribute,
 } from '@vestido-ecommerce/items';
+import { apiRouteHandler } from '@vestido-ecommerce/utils';
 
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
 
-export async function GET(
-  request: Request,
-  { params }: { params: { slug: string } },
-) {
-  try {
-    const attribute = await attributeDetails(params.slug);
-    return new Response(
-      JSON.stringify({
-        success: true,
-        data: attribute,
-      }),
-      {
-        headers: {
-          'content-Type': 'application/json',
-        },
-      },
-    );
-  } catch (e) {
-    console.error(e);
-    return new Response(
-      JSON.stringify({
-        success: false,
-        error: e,
-      }),
-      {
-        status: 500,
-        headers: {
-          'content-Type': 'application/json',
-        },
-      },
-    );
-  }
-}
+export const GET = apiRouteHandler(async ({ params }) => {
+  const attribute = await attributeDetails(params.slug);
+  return attribute;
+});
 
-export async function PUT(
-  request: Request,
-  { params }: { params: { slug: string } },
-) {
+export const PUT = apiRouteHandler(async ({ request, params }) => {
   const data = await request.json();
+  const r = await updateAttribute(params.slug, data);
+  return r;
+});
 
-  try {
-    const r = await updateAttribute(params.slug, data);
-
-    return new Response(
-      JSON.stringify({
-        success: true,
-        data: r,
-      }),
-      {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      },
-    );
-  } catch (e) {
-    if (e instanceof ZodError) {
-      return new Response(
-        JSON.stringify({
-          success: true,
-          error: e,
-        }),
-        {
-          status: 400,
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        },
-      );
-    } else {
-      console.error('Unexpected Error', e);
-      return new Response(
-        JSON.stringify({
-          message: 'Unknown Error',
-        }),
-        {
-          status: 500,
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        },
-      );
-    }
-  }
-}
-
-export async function DELETE(
-  request: Request,
-  { params }: { params: { slug: string } },
-) {
-  try {
-    const deletedAttribute = await deleteAttribute(params.slug);
-
-    return new Response(
-      JSON.stringify({ success: true, deleted: deletedAttribute }),
-      {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      },
-    );
-  } catch (e) {
-    console.error(e);
-    return new Response(JSON.stringify(e), {
-      status: 400,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-  }
-}
+export const DELETE = apiRouteHandler(async ({ params }) => {
+  const deletedAttribute = await deleteAttribute(params.slug);
+  return deletedAttribute;
+});
--- vestido/vestido-dashboard/app/api/attributes/route.ts
+++ vestido/vestido-dashboard/app/api/attributes/route.ts
@@ -1,80 +1,14 @@
-import { type NextRequest } from 'next/server';
+import { createAttribute, listAttribute } from '@vestido-ecommerce/items';
+import { apiRouteHandler } from '@vestido-ecommerce/utils';
 
-import { ZodError } from 'zod';
+export const GET = apiRouteHandler(async ({ request }) => {
+  const args = Object.fromEntries(request.nextUrl.searchParams.entries());
+  const attributes = await listAttribute(args);
+  return attributes;
+});
 
-import { VestidoResponse } from '@vestido-ecommerce/auth';
-import {
-  createAttribute,
-  listAttribute,
-  ListAttributesResponse,
-} from '@vestido-ecommerce/items';
-
-export async function GET(request: NextRequest) {
-  try {
-    const args = Object.fromEntries(request.nextUrl.searchParams.entries());
-    const attributes = await listAttribute(args);
-
-    return new Response(
-      JSON.stringify({
-        success: true,
-        data: attributes,
-      } as VestidoResponse<ListAttributesResponse>),
-      {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      },
-    );
-  } catch (e) {
-    console.error(e);
-    return new Response(
-      JSON.stringify({
-        success: false,
-        error: e,
-      } as VestidoResponse<ListAttributesResponse>),
-      {
-        status: 500,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      },
-    );
-  }
-}
-export async function POST(request: Request) {
-  try {
-    const body = await request.json();
-    const newAttribute = await createAttribute(body);
-
-    return new Response(JSON.stringify({ success: true, data: newAttribute }), {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-  } catch (e) {
-    if (e instanceof ZodError) {
-      return new Response(JSON.stringify({ success: false, error: e }), {
-        status: 400,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-    } else {
-      console.error('Unexpected Error', e);
-      return (
-        new Response(
-          JSON.stringify({
-            success: false,
-            message: 'Unknown Error',
-          }),
-        ),
-        {
-          status: 500,
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-    }
-  }
-}
+export const POST = apiRouteHandler(async ({ request }) => {
+  const body = await request.json();
+  const newAttribute = await createAttribute(body);
+  return newAttribute;
+});
