77984e37d3edaa7884dbdb6e9fa1c0ed8e5779b4
FathimaSayeeda <56033940+FathimaSayeeda@users.noreply.github.com>
2024-10-12 15:30:55 +0530

    feat: feedback on Cart & Wishlist Operations (#132)
    * feat: feedback on Cart & Wishlist Operations
    
    * fix: Separate Toast Description JSX
    
    * fix: Error Adding To Cart Feedback
    
    * chore: Refactor
    
    * chore: Refactor
    
    * fix: Remove Toaster Outlet
    
    * fix: Add Toaster Outlet in the Root
    
    ---------
    
    Co-authored-by: Fahim Ali Zain <fahimalizain@gmail.com>

--- vestido/vestido-storefront/pages/_app.tsx
+++ vestido/vestido-storefront/pages/_app.tsx
@@ -5,6 +5,7 @@ import Head from 'next/head';
 
 import { AuthProvider } from '@vestido-ecommerce/auth/client';
 import { PostHogProvider } from '@vestido-ecommerce/posthog/client';
+import { Toaster } from '@vestido-ecommerce/shadcn-ui/toaster';
 
 import BlockingSpinner from '../components/BlockingSpinner';
 import MainLayout from '../layouts/MainLayout';
@@ -91,6 +92,7 @@ function App({ Component, pageProps }: AppPropsWithLayout) {
           >
             {getLayout(<Component {...pageProps} />)}
           </AuthProvider>
+          <Toaster />
         </div>
       </PostHogProvider>
     </SentryErrorBoundary>
