c36150f1a53ff1fec68cccb5d24eced93cd601f3
Fathima Sayeeda <sayeeda.61911@gmail.com>
2024-10-15 12:43:33 +0530

    feat: Tax Field in Product Form Done

--- vestido/vestido-dashboard/modules/products/product-form-tax-item.tsx
+++ vestido/vestido-dashboard/modules/products/product-form-tax-item.tsx
@@ -4,25 +4,42 @@ import { useFormContext } from 'react-hook-form';
 
 import { useTaxes } from '@vestido-ecommerce/tax/client';
 
+import { CheckBoxElement } from '../../forms/checkbox-element';
+import { InputElement } from '../../forms/input-element';
 import { SelectElement } from '../../forms/select-element';
 import { CreateProductForm } from './zod';
 
 const ProductFormTaxItem: FC = () => {
   const form = useFormContext<CreateProductForm>();
+  const { data: { data: taxes } = { data: null } } = useTaxes();
 
-  const { data } = useTaxes();
-  console.log('taxes', data);
+  console.log(form.getValues());
 
+  const handleTaxSelection = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedTaxTitle = event.target.value;
+    const selectedTax = taxes?.find((tax) => tax.title === selectedTaxTitle);
+    if (selectedTax) {
+      form.setValue('taxRate', selectedTax.rate);
+      form.setValue('taxTitle', selectedTax.title);
+    }
+  };
   return (
     <div>
-      <SelectElement
-        name="taxTitle"
-        options={[
-          { title: 'tax1', id: 'TAX_1' },
-          { title: 'tax2', id: 'TAX_2' },
-        ]}
-        placeholder="Select Tax"
-      ></SelectElement>
+      {taxes && (
+        <>
+          <SelectElement
+            options={taxes.map((tax) => ({ title: tax.title, id: tax.title }))}
+            placeholder="Select Tax"
+            {...form.register('taxTitle', {
+              onChange: (event) => {
+                handleTaxSelection(event);
+              },
+            })}
+          />
+          <InputElement name="taxRate" readOnly />
+        </>
+      )}
+      <CheckBoxElement name="taxInclusive" label="Tax Inclusive" />
     </div>
   );
 };
