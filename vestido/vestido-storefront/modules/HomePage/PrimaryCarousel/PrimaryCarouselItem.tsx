f2233e1868f75bc0777228e63854637b6e05f877
Fathima Sayeeda <sayeeda.61911@gmail.com>
2024-11-07 19:22:16 +0530

    fix: carousel text hidden in small screens

--- vestido/vestido-storefront/modules/HomePage/PrimaryCarousel/PrimaryCarouselItem.tsx
+++ vestido/vestido-storefront/modules/HomePage/PrimaryCarousel/PrimaryCarouselItem.tsx
@@ -21,37 +21,35 @@ interface PrimaryCarouselItemProps {
 }
 const PrimaryCarouselItem: React.FC<PrimaryCarouselItemProps> = ({ data }) => {
   return (
-    <CarouselItem className="w-full">
-      <div className="relative">
-        <div className="min-h-[300px] md:min-h-[500px] xl:min-h-[600px] overflow-hidden w-full">
-          <Image
-            className="object-cover lg:rounded-[25px]"
-            src={data.backgroundImage}
-            alt="Your alt text"
-            fill
-          />
-        </div>
+    <CarouselItem className="w-full relative">
+      <div className="min-h-[300px] md:min-h-[500px] xl:min-h-[600px] overflow-hidden w-full">
+        <Image
+          className="object-cover lg:rounded-[25px]"
+          src={data.backgroundImage}
+          alt="Your alt text"
+          fill
+        />
+      </div>
+      <div
+        className={`flex flex-col gap-1 absolute mt-10 md:mt-auto md:top-1/3 ${data.textPosition} text-${data.textAlign} z-20`}
+      >
         <div
-          className={`flex flex-col gap-1 absolute mt-10 md:mt-auto md:top-1/3 ${data.textPosition} text-${data.textAlign} z-20`}
+          className={`uppercase text-xs md:text-base font-bold md:font-extrabold text-${data.textColor}`}
         >
-          <div
-            className={`uppercase text-xs md:text-base font-bold md:font-extrabold text-${data.textColor}`}
-          >
-            {data.subtitle1}
-          </div>
-          <h1
-            className={`capitalize text-lg md:text-5xl max-w-[500px] leading-normal text-${data.textColor}`}
-          >
-            {data.mainTitle}
-          </h1>
-          <div
-            className={`text-xs font-light md:text-base md:font-extralight text-${data.textColor}`}
-          >
-            {data.subtitle2}
-          </div>
-          <div>
-            <DiscoverButton />
-          </div>
+          {data.subtitle1}
+        </div>
+        <h1
+          className={`capitalize text-lg md:text-5xl max-w-[500px] leading-normal text-${data.textColor}`}
+        >
+          {data.mainTitle}
+        </h1>
+        <div
+          className={`text-xs font-light md:text-base md:font-extralight text-${data.textColor}`}
+        >
+          {data.subtitle2}
+        </div>
+        <div>
+          <DiscoverButton />
         </div>
       </div>
     </CarouselItem>
