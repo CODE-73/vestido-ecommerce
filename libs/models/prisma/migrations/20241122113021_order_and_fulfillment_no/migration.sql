2c4177604636147ae021a3e363e4d12f276f4b9a
Fathima Sayeeda <sayeeda.61911@gmail.com>
2024-11-22 17:34:25 +0530

    feat: Starting Value for Order & fulfillment No

--- libs/models/prisma/migrations/20241122113021_order_and_fulfillment_no/migration.sql
+++ libs/models/prisma/migrations/20241122113021_order_and_fulfillment_no/migration.sql
@@ -4,5 +4,3 @@ ADD COLUMN "fulfillment_no" BIGSERIAL NOT NULL;
 -- AlterTable
 ALTER TABLE "Order"
 ADD COLUMN "order_no" BIGSERIAL NOT NULL;
-ALTER SEQUENCE "Order_order_no-seq" RESTART WITH 10001;
-ALTER SEQUENCE "Fulfillment_fulfillment_no_seq" RESTART WITH 101;
