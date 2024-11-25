8236c78a3a9cf4f2866d3e811904e92bcf68e96b
Fathima Sayeeda <sayeeda.61911@gmail.com>
2024-11-26 01:09:58 +0530

    fix: HomePage Button Color

--- vestido/vestido-storefront/modules/HomePage/Buttons/DiscoverButton.tsx
+++ vestido/vestido-storefront/modules/HomePage/Buttons/DiscoverButton.tsx
@@ -3,14 +3,16 @@ import clsx from 'clsx';
 import { Button } from '@vestido-ecommerce/shadcn-ui/button';
 type DiscoverButtonProps = {
   className?: string;
+  bgLight?: boolean;
 };
 
 export const DiscoverButton: React.FC<DiscoverButtonProps> = ({
   className,
+  bgLight,
 }) => {
   return (
     <Button
-      className={`mt-6 h-10 sm:h-12 rounded-none bg-black text-white  uppercase font-bold tracking-wide px-10 ${clsx(className)}`}
+      className={`mt-6 h-10 sm:h-12 rounded-none  uppercase font-bold tracking-wide px-10 ${clsx(className)} ${bgLight ? 'bg-white text-black hover:border hover:border-white hover:text-white' : 'bg-black text-white'}`}
     >
       discover now!
     </Button>
