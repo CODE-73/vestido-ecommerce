0511c4ebf8e30539d4edbe2bbef5b1f2b9bfd092
FathimaSayeeda <56033940+FathimaSayeeda@users.noreply.github.com>
2024-11-26 09:57:57 +0530

    fix: Cart Item Component with Size Display (#175)

--- vestido/vestido-storefront/modules/Cart/CartView.tsx
+++ vestido/vestido-storefront/modules/Cart/CartView.tsx
@@ -3,16 +3,9 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
-import {
-  LuChevronLeft,
-  LuChevronRight,
-  LuMinus,
-  LuPlus,
-  LuTrash2,
-} from 'react-icons/lu';
+import { LuChevronLeft, LuChevronRight, LuTrash2 } from 'react-icons/lu';
 
 import {
-  useAddToCart,
   useAddToWishlist,
   useCart,
   useRemoveFromCart,
@@ -32,16 +25,14 @@ import { Button } from '@vestido-ecommerce/shadcn-ui/button';
 import { toast } from '@vestido-ecommerce/shadcn-ui/use-toast';
 import { formatINR } from '@vestido-ecommerce/utils';
 
-import ItemImage from '../../components/item-image';
 import { ItemToastBody } from '../../components/item-toast-body';
+import CartItemCard from './CartItem';
 
 const CartView: React.FC = () => {
   const router = useRouter();
   const { data: { data: cartItems } = { data: [] } } = useCart();
 
   const { trigger } = useRemoveFromCart();
-
-  const { trigger: addCartTrigger } = useAddToCart();
   const { trigger: wishlistTrigger } = useAddToWishlist();
 
   const removeItem = (itemId: string) => {
@@ -83,13 +74,6 @@ const CartView: React.FC = () => {
     }
   };
 
-  const handleAddToCart = (itemId: string, qty: number, variantId: string) => {
-    addCartTrigger({
-      itemId: itemId,
-      qty: qty,
-      variantId: variantId,
-    });
-  };
   const handleAddToWishlist = (itemId: string) => {
     wishlistTrigger({
       itemId: itemId,
@@ -135,119 +119,11 @@ const CartView: React.FC = () => {
           <div className="md:grow flex flex-col ">
             {cartItems.map((cartItem, index) => {
               return (
-                <div key={index}>
-                  <div className="flex gap-2 lg:gap-4 items-center bg-neutral-800 border border-gray-600 mb-5 min-h-[170px]  md:rounded-lg  relative">
-                    <div className="absolute right-1 md:right-5 top-1 md:top-5">
-                      <AlertDialog>
-                        <AlertDialogTrigger asChild className="cursor-pointer">
-                          <LuTrash2 color="rgb(161 161 170)" size={20} />
-                        </AlertDialogTrigger>
-                        <AlertDialogContent>
-                          <AlertDialogHeader>
-                            <AlertDialogTitle>
-                              Are you sure you want to delete this item from
-                              cart?
-                            </AlertDialogTitle>
-                            <AlertDialogDescription>
-                              You can move this to wishlist for future.
-                            </AlertDialogDescription>
-                          </AlertDialogHeader>
-                          <AlertDialogFooter>
-                            <AlertDialogCancel>Cancel</AlertDialogCancel>
-
-                            <AlertDialogAction
-                              onClick={() =>
-                                handleRemoveFromCart(
-                                  cartItem.itemId,
-                                  cartItem.variantId!,
-                                  'full',
-                                )
-                              }
-                            >
-                              Remove
-                            </AlertDialogAction>
-                            <AlertDialogAction
-                              onClick={() => {
-                                handleRemoveFromCart(
-                                  cartItem.itemId,
-                                  cartItem.variantId ?? '',
-                                  'full',
-                                );
-                                handleAddToWishlist(cartItem.itemId);
-                              }}
-                            >
-                              Move to wishlist
-                            </AlertDialogAction>
-                          </AlertDialogFooter>
-                        </AlertDialogContent>
-                      </AlertDialog>
-                    </div>
-                    <Link
-                      href={`/products/${cartItem.itemId}`}
-                      className="block basis-1/5 md:basis-1/4"
-                    >
-                      <ItemImage
-                        item={cartItem.item}
-                        width={150}
-                        height={195}
-                        className="pl-2 md:pl-0 md:rounded-l-lg"
-                      />
-                    </Link>
-
-                    <div className="flex flex-col gap-1 lg:gap-3 md:self-start my-4">
-                      <div className="md:text-xl font-semibold text-white">
-                        {cartItem.item.title}
-                      </div>
-
-                      <div className="flex flex-row text-neutral-500 border border-neutral-500 rounded-full  w-[100px] items-center justify-between  px-2 ">
-                        <div
-                          className="text-zinc-300 cursor-pointer"
-                          onClick={() => {
-                            handleRemoveFromCart(
-                              cartItem.itemId,
-                              cartItem.variantId!,
-                              'decrement',
-                            );
-                          }}
-                        >
-                          <LuMinus />
-                        </div>
-                        <div className="font-semibold ">{cartItem.qty}</div>
-                        <div
-                          className=" cursor-pointer"
-                          onClick={() => {
-                            handleAddToCart(
-                              cartItem.itemId,
-                              cartItem.qty + 1,
-                              cartItem.variantId!,
-                            );
-                          }}
-                        >
-                          <LuPlus />
-                        </div>
-                      </div>
-                      <div className="  col-span-3">
-                        {cartItem.item.discountedPrice ? (
-                          <div className="flex items-center gap-2">
-                            <div className="text-white text-sm font-semibold">
-                              {formatINR(cartItem.item.discountedPrice)}
-                            </div>
-                            {cartItem.item.discountedPrice <
-                            cartItem.item.price ? (
-                              <div className="text-white line-through text-xs">
-                                {formatINR(cartItem.item.price)}
-                              </div>
-                            ) : (
-                              ''
-                            )}
-                          </div>
-                        ) : (
-                          <div> {formatINR(cartItem.item.price)}</div>
-                        )}
-                      </div>
-                    </div>
-                  </div>
-                </div>
+                <CartItemCard
+                  key={index}
+                  cartItem={cartItem}
+                  removeItem={removeItem}
+                />
               );
             })}
             <div className="border-t border-gray-300 my-4"></div>
