db53940c758d36cf702a78df4ae541c885eb95cc
Fathima Sayeeda <sayeeda.61911@gmail.com>
2024-11-28 17:38:13 +0530

    fix: Wishlist View

--- vestido/vestido-storefront/modules/Wishlist/WishlistView.tsx
+++ vestido/vestido-storefront/modules/Wishlist/WishlistView.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 import { LuShoppingBag, LuX } from 'react-icons/lu';
 
@@ -21,14 +22,17 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from '@vestido-ecommerce/shadcn-ui/alert-dialog';
+import { Badge } from '@vestido-ecommerce/shadcn-ui/badge';
 import { Button } from '@vestido-ecommerce/shadcn-ui/button';
 import { useToast } from '@vestido-ecommerce/shadcn-ui/use-toast';
-import { formatINR, ImageSchemaType } from '@vestido-ecommerce/utils';
+import { formatINR } from '@vestido-ecommerce/utils';
 
+import ItemImage from '../../components/item-image';
 import { ItemToastBody } from '../../components/item-toast-body';
 import { SizeSelectorDialog } from './size-selector';
 
 const WishlistView: React.FC = () => {
+  const router = useRouter();
   const { data: { data: wishlistItems } = { data: [] } } = useWishlist();
 
   const { trigger: wishlistTrigger } = useRemoveFromWishlist();
@@ -88,6 +92,10 @@ const WishlistView: React.FC = () => {
     }
   };
 
+  const handleProductClick = (itemId: string) => {
+    router.push(`/products/${encodeURIComponent(itemId)}`);
+  };
+
   return (
     <div className="md:px-16">
       <div className="text-4xl flex items-center justify-center gap-3 tracking-wide text-white text-center font-extrabold my-5 lg:py-10">
@@ -97,70 +105,95 @@ const WishlistView: React.FC = () => {
         </span>
       </div>
       {wishlistItems.length && wishlistItems.length > 0 ? (
-        <div className="grid grid-cols-2 lg:grid-cols-4 gap-10">
+        <div className=" grid grid-cols-2 gap-2 px-5 md:grid-cols-3 lg:grid-cols-4  md:gap-5 xl:grid-cols-6  xl:gap-10 md:px-0">
           {wishlistItems.map((wishlistItem, index) => {
-            const img =
-              ((wishlistItem.item.images ?? []) as ImageSchemaType[])?.[0] ||
-              null;
             return (
               <div
                 key={index}
-                className=" flex flex-col items-center group relative mb-10 "
+                className="relative flex flex-col items-center group  mb-10 cursor-pointer "
               >
-                <div>
-                  <div className="absolute right-3 top-3 text-stone-400 border border-1 border-stone-400 rounded-full p-1 cursor-pointer">
-                    <AlertDialog>
-                      <AlertDialogTrigger asChild>
-                        <LuX />
-                      </AlertDialogTrigger>
-                      <AlertDialogContent>
-                        <AlertDialogHeader>
-                          <AlertDialogTitle>
-                            Are you sure you want to remove this from wishlist?
-                          </AlertDialogTitle>
-                        </AlertDialogHeader>
-                        <AlertDialogFooter>
-                          <AlertDialogCancel>Cancel</AlertDialogCancel>
+                {wishlistItem.item.discountPercent &&
+                wishlistItem.item.discountPercent > 0 ? (
+                  <Badge className="absolute top-2 left-2 rounded-none uppercase bg-red-500 hover:bg-red-400 cursor-auto z-10">
+                    sale&nbsp;{wishlistItem.item.discountPercent}&nbsp;%
+                  </Badge>
+                ) : (
+                  ''
+                )}
+                <div className="absolute z-10 right-3 top-3 text-stone-400 border border-2 border-stone-400 rounded-full p-1 cursor-pointer">
+                  <AlertDialog>
+                    <AlertDialogTrigger asChild>
+                      <LuX strokeWidth={3} />
+                    </AlertDialogTrigger>
+                    <AlertDialogContent>
+                      <AlertDialogHeader>
+                        <AlertDialogTitle>
+                          Are you sure you want to remove this from wishlist?
+                        </AlertDialogTitle>
+                      </AlertDialogHeader>
+                      <AlertDialogFooter>
+                        <AlertDialogCancel>Cancel</AlertDialogCancel>
 
-                          <AlertDialogAction
-                            onClick={(e) => {
-                              e.stopPropagation();
-                              handleRemoveFromWishlist(wishlistItem.itemId);
-                            }}
-                          >
-                            Yes, Remove.
-                          </AlertDialogAction>
-                        </AlertDialogFooter>
-                      </AlertDialogContent>
-                    </AlertDialog>
-                  </div>
-                  <Link href={`/products/${wishlistItem.itemId}`}>
-                    <Image
-                      className="block col-span-2"
-                      src={img.url ?? ''}
-                      alt={img.alt ?? ''}
-                      width={250}
-                      height={300}
-                      placeholder={img.blurHashDataURL ? 'blur' : undefined}
-                      blurDataURL={img.blurHashDataURL ?? undefined}
-                    />
-                  </Link>
+                        <AlertDialogAction
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            handleRemoveFromWishlist(wishlistItem.itemId);
+                          }}
+                        >
+                          Yes, Remove.
+                        </AlertDialogAction>
+                      </AlertDialogFooter>
+                    </AlertDialogContent>
+                  </AlertDialog>
                 </div>
 
-                <div className="self-start pt-[#1px] capitalize text-[#333333] text-md font-thin">
+                <div
+                  onClick={() => handleProductClick(wishlistItem.itemId)}
+                  className="group w-full relative"
+                >
+                  <div className="relative w-full pb-[130%]">
+                    <ItemImage
+                      item={wishlistItem.item}
+                      style={{ objectFit: 'cover' }}
+                      className="absolute inset-0 block group-hover:hidden object-cover"
+                      fill
+                    />
+                    <ItemImage
+                      item={wishlistItem.item}
+                      style={{ objectFit: 'cover' }}
+                      className="absolute inset-0 group-hover:block hidden object-cover"
+                      imageIdx={1}
+                      fill
+                    />
+                  </div>
+                </div>
+
+                <div className="self-start pt-[#2px] capitalize text-white text-xs md:text-base font-light md:mb-4 w-full truncate">
                   {wishlistItem.item.title}
                 </div>
 
-                <>
-                  <div className="flex text-lg justify-between w-full pt-1">
-                    <div>
-                      {formatINR(
-                        wishlistItem.item.discountedPrice ??
-                          wishlistItem.item.price,
+                <div className="self-start mb-4">
+                  {wishlistItem.item.discountedPrice ? (
+                    <div className="flex items-center gap-2">
+                      <div className="text-white text-sm font-semibold">
+                        {formatINR(wishlistItem.item.discountedPrice)}
+                      </div>
+                      {wishlistItem.item.discountedPrice <
+                      wishlistItem.item.price ? (
+                        <div className="text-white line-through text-xs">
+                          {formatINR(wishlistItem.item.price)}
+                        </div>
+                      ) : (
+                        ''
                       )}
                     </div>
-                  </div>
-
+                  ) : (
+                    <div className="text-white text-sm font-semibold">
+                      {formatINR(wishlistItem.item.price)}
+                    </div>
+                  )}
+                </div>
+                <div className=" w-full self-start">
                   <SizeSelectorDialog
                     itemId={wishlistItem.itemId}
                     onSizeSelect={(selectedVariantId) => {
@@ -168,14 +201,14 @@ const WishlistView: React.FC = () => {
                       handleAddToCart(wishlistItem, selectedVariantId);
                     }}
                   >
-                    <div className={`p-2 bg-white w-full`}>
-                      <Button className="bg-white w-full flex gap-3 h-[30px] text-lg mb-1 text-white p-2 uppercase font-semibold tracking-wide hover:bg-transparent">
-                        <LuShoppingBag color="#fff" size={24} />
+                    <div className="p-2 bg-white w-full rounded-lg">
+                      <Button className="bg-white w-full flex gap-3 h-[30px] text-black rounded-lg p-2  font-semibold tracking-wide hover:bg-transparent">
+                        <LuShoppingBag size={20} />
                         <div> Add to Cart</div>
                       </Button>
                     </div>
                   </SizeSelectorDialog>
-                </>
+                </div>
               </div>
             );
           })}
