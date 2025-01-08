0c50998d9bf8c341ae50fdad765e3e6391da005c
Fathima Sayeeda <sayeeda.61911@gmail.com>
2025-01-08 04:55:11 +0000

    fix: Unused Code

--- vestido/vestido-storefront/modules/ProductListView/ProductTile.tsx
+++ vestido/vestido-storefront/modules/ProductListView/ProductTile.tsx
@@ -1,4 +1,3 @@
-// import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 
 import { Item } from '@prisma/client';
@@ -13,12 +12,10 @@ import AddToWishListButton from './AddToWishlistButton';
 
 interface ProductCardProps {
   data: Item;
-  // animeIdx: number;
 }
 
 const ProductCard: React.FC<ProductCardProps> = ({ data: item }) => {
   const router = useRouter();
-  // const mounted = useMountAnimation(animeIdx);
   const handleProductClick = (itemId: string) => {
     router.push(`/products/${encodeURIComponent(itemId)}`);
   };
