61c925ee041d4c61da7bb427deb18c84e38666eb
Mohammed Fabinsha <fabinsha13@gmail.com>
2025-01-08 11:37:15 +0530

    fix: timestamp fields

--- libs/models/prisma/migrations/20250108040655_exchange_to_replacement_schema/migration.sql
+++ libs/models/prisma/migrations/20250108040655_exchange_to_replacement_schema/migration.sql
@@ -1,32 +1,22 @@
 /*
-  Warnings:
-
-  - You are about to drop the column `createdAt` on the `Item` table. All the data in the column will be lost.
-  - You are about to drop the column `updatedAt` on the `Item` table. All the data in the column will be lost.
-  - You are about to drop the column `exchangeStatus` on the `Order` table. All the data in the column will be lost.
-  - You are about to drop the column `exchangeStatus` on the `OrderItem` table. All the data in the column will be lost.
-
-*/
+ Warnings:
+ 
+ - You are about to drop the column `exchangeStatus` on the `Order` table. All the data in the column will be lost.
+ - You are about to drop the column `exchangeStatus` on the `OrderItem` table. All the data in the column will be lost.
+ 
+ */
 -- CreateEnum
-CREATE TYPE "ReplacementStatus" AS ENUM ('REPLACEMENT_REQUESTED', 'REPLACEMENT_IN_PROGRESS', 'PARTIALLY_REPLACED', 'REPLACED');
-
--- AlterTable
-ALTER TABLE "CartItem" ALTER COLUMN "createdAt" DROP NOT NULL;
-
--- AlterTable
-ALTER TABLE "Item" DROP COLUMN "createdAt",
-DROP COLUMN "updatedAt";
-
+CREATE TYPE "ReplacementStatus" AS ENUM (
+  'REPLACEMENT_REQUESTED',
+  'REPLACEMENT_IN_PROGRESS',
+  'PARTIALLY_REPLACED',
+  'REPLACED'
+);
 -- AlterTable
 ALTER TABLE "Order" DROP COLUMN "exchangeStatus",
-ADD COLUMN     "replacementStatus" "ReplacementStatus";
-
+  ADD COLUMN "replacementStatus" "ReplacementStatus";
 -- AlterTable
 ALTER TABLE "OrderItem" DROP COLUMN "exchangeStatus",
-ADD COLUMN     "replacementStatus" "ReplacementStatus";
-
--- AlterTable
-ALTER TABLE "WishlistItem" ALTER COLUMN "createdAt" DROP NOT NULL;
-
+  ADD COLUMN "replacementStatus" "ReplacementStatus";
 -- DropEnum
 DROP TYPE "ExchangeStatus";
