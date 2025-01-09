cdbd307d90ba6286c069fb7d509ab8c5f02747c6
FathimaSayeeda <56033940+FathimaSayeeda@users.noreply.github.com>
2025-01-09 17:51:56 +0530

    fix: Redirecting If No Link Present (#209)
    * fix: CategoryElement replaced with InputElement
    
    * fix: Rename CategoryCard and HeroCategories to CircleLinks
    
    * fix: Rename in Update & Format Services
    
    * fix: Carousel Text Positions & Href in Schema
    
    * feat: href fields in form
    
    * fix: Href in Storefront Cards
    
    * fix: Redirecting if No Link Present
    
    * fix: Duplicate Imports
    
    * fix: pointer-events-none Class Applied

--- vestido/vestido-storefront/modules/HomePage/CircleLinks/CircleLink.tsx
+++ vestido/vestido-storefront/modules/HomePage/CircleLinks/CircleLink.tsx
@@ -4,17 +4,17 @@ import Link from 'next/link';
 import { z } from 'zod';
 
 import { CircleLinksSchema } from '@vestido-ecommerce/settings/client';
-
 type CircleLinkData = z.infer<typeof CircleLinksSchema>;
-
 interface CircleLinkProps {
   data: CircleLinkData;
 }
-
 const CircleLink: React.FC<CircleLinkProps> = ({ data }) => {
   return (
     <div className="group basis-1/4 lg:basis-48 text-center transition duration-700 ease-in-out md:hover:-translate-y-5 text-[#333333]">
-      <Link href={`/${data.href}`}>
+      <Link
+        href={`/${data.href}`}
+        className={`${data.href ? '' : 'pointer-events-none'}`}
+      >
         <div className="flex flex-col items-center mb-5 md:mb-auto">
           <div className="relative w-16 h-16 md:w-32 md:h-32 overflow-hidden rounded-full">
             <Image
@@ -27,7 +27,7 @@ const CircleLink: React.FC<CircleLinkProps> = ({ data }) => {
             />
           </div>
 
-          <div className="pt-2 capitalize text-center text-xs md:text-base group-hover:underline group-hover:underline-offset-4 leading-normal text-white group-hover:text-[#48CAB2] ">
+          <div className="pt-2 capitalize text-center text-xs md:text-base leading-normal text-white group-hover:text-[#48CAB2] ">
             {data.text_content}
           </div>
         </div>
