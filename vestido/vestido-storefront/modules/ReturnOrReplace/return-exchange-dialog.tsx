1c0369f2b5bb8cb58d5e99f71ecfc09a849b892a
Fathima Sayeeda <sayeeda.61911@gmail.com>
2025-01-23 02:46:33 +0000

    WIP: Return Form Submission

--- vestido/vestido-storefront/modules/ReturnOrReplace/return-exchange-dialog.tsx
+++ vestido/vestido-storefront/modules/ReturnOrReplace/return-exchange-dialog.tsx
@@ -2,19 +2,19 @@ import { ReactNode, useState } from 'react';
 
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
+import { LuMinus, LuPlus } from 'react-icons/lu';
 import { z } from 'zod';
 
 import { GetOrderResponse } from '@vestido-ecommerce/orders';
+import { ReturnOrderSchema } from '@vestido-ecommerce/orders';
+import { useCreateReturnOrder } from '@vestido-ecommerce/orders/client';
 import { Button } from '@vestido-ecommerce/shadcn-ui/button';
 import {
   Card,
-  CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from '@vestido-ecommerce/shadcn-ui/card';
-import { Checkbox } from '@vestido-ecommerce/shadcn-ui/checkbox';
 import {
   Dialog,
   DialogContent,
@@ -48,7 +48,7 @@ const ReturnReplaceFormSchema = z.object({
   account_number: z.string().nullish(),
   ifsc_code: z.string().nullish(),
 });
-export type ReturnReplaceForm = z.infer<typeof ReturnReplaceFormSchema>;
+export type ReturnReplaceForm = z.infer<typeof ReturnOrderSchema>;
 
 const ReturnReplaceDialog: React.FC<ReturnReplaceDialogProps> = ({
   children,
@@ -57,6 +57,8 @@ const ReturnReplaceDialog: React.FC<ReturnReplaceDialogProps> = ({
 }) => {
   const cod = order?.payments[0].paymentGateway === 'CASH_ON_DELIVERY';
   const { toast } = useToast();
+
+  const { trigger } = useCreateReturnOrder();
 
   const [activeDialog, setActiveDialog] = useState<string>('selectItems');
   const [isDialogOpen, setIsDialogOpen] = useState(false);
@@ -68,15 +70,46 @@ const ReturnReplaceDialog: React.FC<ReturnReplaceDialogProps> = ({
 
   const form = useForm<ReturnReplaceForm>({
     resolver: zodResolver(ReturnReplaceFormSchema),
-    defaultValues: { orderId: order?.id, orderItems: [] },
+    defaultValues: {
+      returnType: 'RETURN',
+      orderId: order?.id,
+      returnItems: [{ quantity: 0 }],
+      reason: 'Other',
+      fulfillmentId: 'trial',
+    },
   });
 
-  const handleSubmit = async (/* data: ReturnReplaceForm */) => {
-    await toast({
-      title: `${isReturn ? 'Return' : 'Exchange'} Request Placed Successfully.`,
-    });
-    setActiveDialog('confirmation');
+  console.info(
+    form.formState.isValid,
+    form.formState.isDirty,
+    form.formState.errors,
+    form.getValues(),
+  );
+
+  const returnItems = form.watch('returnItems') || [];
+
+  // Dynamically filter items with quantities > 0
+  const filteredReturnItems = returnItems.filter((item) => item.quantity > 0);
+
+  console.log('Filtered Return Items:', filteredReturnItems);
+
+  const handleSubmit = async (data: ReturnReplaceForm) => {
+    console.log('handlesubmit return');
+    try {
+      await trigger({
+        ...data,
+        returnItems: filteredReturnItems,
+        fulfillmentId: 'jghgh',
+      });
+      toast({
+        title: `${isReturn ? 'Return' : 'Exchange'} Request Placed Successfully.`,
+      });
+      setActiveDialog('confirmation');
+    } catch (e) {
+      console.error('Error creating return order', e);
+    }
   };
+
   return (
     <Dialog
       open={isDialogOpen}
@@ -111,9 +144,8 @@ const ReturnReplaceDialog: React.FC<ReturnReplaceDialogProps> = ({
                   )}
                 </DialogDescription>
 
-                {order?.orderItems.map((orderItem, index) => (
+                {/* {order?.orderItems.map((orderItem, index) => (
                   <div key={index} className="py-3 grid grid-cols-10">
-                    <Checkbox />
                     <ItemImage
                       item={orderItem.item}
                       width={50}
@@ -123,11 +155,102 @@ const ReturnReplaceDialog: React.FC<ReturnReplaceDialogProps> = ({
                     <div className="text-xs col-span-6 pl-1">
                       {orderItem.item.title}
                     </div>
-                    <div className="px-1 text-sm text-center justify-self-center">
-                      {orderItem.qty}
+
+                    <div className="flex flex-row text-neutral-500 border border-neutral-500 rounded-full  w-[100px] items-center justify-between  px-2 ">
+                      <div className="text-zinc-300 cursor-pointer">
+                        <LuMinus />
+                      </div>
+                      <div className="font-semibold ">{orderItem.qty}</div>
+                      <div className=" cursor-pointer">
+                        <LuPlus />
+                      </div>
                     </div>
                   </div>
-                ))}
+                ))} */}
+                {order?.orderItems.map((orderItem, index) => {
+                  return (
+                    <div key={index} className="py-3 grid grid-cols-10">
+                      <ItemImage
+                        item={orderItem.item}
+                        width={50}
+                        height={70}
+                        className="w-10 h-12 justify-self-center col-span-2"
+                      />
+                      <div className="text-xs col-span-6 pl-1">
+                        {orderItem.item.title}
+                      </div>
+
+                      {/* Quantity selector */}
+                      <div className="flex flex-row text-neutral-500 border border-neutral-500 rounded-full w-[100px] items-center justify-between px-2">
+                        {/* Decrement Button */}
+                        <div
+                          className={`text-zinc-300 cursor-pointer`}
+                          onClick={() => {
+                            const currentQty =
+                              form.getValues(`returnItems.${index}.quantity`) ||
+                              0;
+                            if (currentQty > 0) {
+                              form.setValue(
+                                `returnItems.${index}.quantity`,
+                                currentQty - 1,
+                              );
+                            }
+                          }}
+                        >
+                          <LuMinus />
+                        </div>
+
+                        {/* Quantity Input */}
+                        <InputElement
+                          name={`returnItems.${index}.quantity`}
+                          type="number"
+                          validation={{
+                            min: 0,
+                            max: orderItem.qty,
+                          }}
+                          wrapperClassName="w-6 text-center bg-transparent border-none"
+                          onChange={(e) => {
+                            const value = Number(e.target.value);
+                            if (value >= 0 && value <= orderItem.qty) {
+                              form.setValue(
+                                `returnItems.${index}.quantity`,
+                                value,
+                              );
+                            }
+                          }}
+                        />
+
+                        {/* Increment Button */}
+                        <div
+                          className={`cursor-pointer ${
+                            form.getValues(`returnItems.${index}.quantity`) >=
+                            orderItem.qty
+                              ? 'pointer-events-none opacity-50'
+                              : ''
+                          }`}
+                          onClick={() => {
+                            const currentQty =
+                              form.getValues(`returnItems.${index}.quantity`) ||
+                              0;
+                            form.setValue(
+                              `returnItems.${index}.orderItemId`,
+                              orderItem.id,
+                            );
+                            if (currentQty < orderItem.qty) {
+                              form.setValue(
+                                `returnItems.${index}.quantity`,
+                                currentQty + 1,
+                              );
+                            }
+                          }}
+                        >
+                          <LuPlus />
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+
                 <DialogFooter>
                   {isReturn && cod ? (
                     <Button
