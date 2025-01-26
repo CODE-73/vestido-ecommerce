eac11f36d7fb9f0b8f4d8b0c3e390da2dfca791f
Fathima Sayeeda <sayeeda.61911@gmail.com>
2025-01-26 07:38:13 +0000

    fix: Storefront Data Service

--- libs/settings/src/services/storefront-data/format-storefront-data.ts
+++ libs/settings/src/services/storefront-data/format-storefront-data.ts
@@ -22,7 +22,11 @@ export const formatStorefrontData = async (
   const data = p.data;
 
   const images: ImageSchemaType[] = [
-    ...data.hero_carousel.map((x) => x.image),
+    ...(data.hero_carousel.flatMap((x) =>
+      ['sm', 'md', 'lg']
+        .map((k) => x.image[k as keyof typeof x.image])
+        .filter(Boolean),
+    ) as ImageSchemaType[]),
     ...data.circle_links.map((x) => x.image),
     ...data.horizontal_scroll_cards.map((x) => x.image),
     ...data.collage.map((x) => x.image),
