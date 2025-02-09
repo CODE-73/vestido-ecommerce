f3bec114da45c0b19e1602a84f6f7b494cdde798
Fathima Sayeeda <sayeeda.61911@gmail.com>
2025-02-09 11:28:07 +0000

    fix: Build Error

--- libs/orders/src/client.ts
+++ libs/orders/src/client.ts
@@ -1,4 +1,4 @@
-export { GetOrderResponse } from '../../orders/src/services/orders/get-order/types';
-export { GetReturnableitemsResponse } from '../../orders/src/services/return/get-returnable-items/types';
+export type { GetOrderResponse } from '../../orders/src/services/orders/get-order/types';
+export type { GetReturnableitemsResponse } from '../../orders/src/services/return/get-returnable-items/types';
 export { ReturnOrderSchema } from '../../orders/src/services/return/return-order/zod';
 export * from './swr';
