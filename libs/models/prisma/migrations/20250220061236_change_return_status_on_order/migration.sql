1d9c0e95d2a07cdb6f88a5cfc5e935a518be4a8c
Mohammed Fabinsha <fabinsha13@gmail.com>
2025-02-20 11:43:48 +0530

    feat: Introduce OrderReturnStatus to Order & OrderItem

---
+++ libs/models/prisma/migrations/20250220061236_change_return_status_on_order/migration.sql
@@ -0,0 +1,17 @@
+/*
+  Warnings:
+
+  - The `returnStatus` column on the `Order` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+  - The `returnStatus` column on the `OrderItem` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+
+*/
+-- CreateEnum
+CREATE TYPE "OrderReturnStatus" AS ENUM ('NULL', 'RETURN_REQUESTED', 'RETURN_IN_PROGRESS', 'PARTIALLY_RETURNED', 'RETURNED');
+
+-- AlterTable
+ALTER TABLE "Order" DROP COLUMN "returnStatus",
+ADD COLUMN     "returnStatus" "OrderReturnStatus";
+
+-- AlterTable
+ALTER TABLE "OrderItem" DROP COLUMN "returnStatus",
+ADD COLUMN     "returnStatus" "OrderReturnStatus";
