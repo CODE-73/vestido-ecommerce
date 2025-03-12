5de8238a8178c593d82336c300ea289187da7fc3
Fathima Sayeeda <sayeeda.61911@gmail.com>
2025-03-12 06:23:27 +0530

    fix: Framer Page Routing Delay

--- vestido/vestido-storefront/pages/_app.tsx
+++ vestido/vestido-storefront/pages/_app.tsx
@@ -42,7 +42,7 @@ function App({ Component, pageProps }: AppPropsWithLayout) {
               initial={{ opacity: 0, y: 10 }}
               animate={{ opacity: 1, y: 0 }}
               exit={{ opacity: 0, y: -10 }}
-              transition={{ duration: 0.5, ease: 'easeOut' }}
+              transition={{ duration: 0.2, ease: 'easeOut' }}
             >
               {getLayout(<Component {...pageProps} />)}
             </motion.div>
