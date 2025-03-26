eba3826ded27028d5d59157115fdbe86aba7831c
Fathima Sayeeda <sayeeda.61911@gmail.com>
2025-03-26 12:04:21 +0530

    fix: Include AuthHeaders in Get Variant SWR

--- libs/items/src/swr/variants/get-variant/get-variant.ts
+++ libs/items/src/swr/variants/get-variant/get-variant.ts
@@ -1,10 +1,13 @@
 import useSWRImmutable from 'swr/immutable';
 
+import { useAuth } from '@vestido-ecommerce/auth/client';
+
 import { VariantDetailsResponse } from '../../../services/variants/get-variant/types';
 import { VariantDetailsSWRKeys } from '../keys';
 import { getVariantDetails } from './service';
 
 export function useVariant(itemId: string, variantId?: string | null) {
+  const { authHeaders } = useAuth();
   const key = variantId
     ? [
         VariantDetailsSWRKeys.VARIANT,
@@ -16,7 +19,7 @@ export function useVariant(itemId: string, variantId?: string | null) {
 
   return useSWRImmutable<VariantDetailsResponse, Error>(
     key,
-    () => getVariantDetails(variantId as string, itemId),
+    () => getVariantDetails(variantId as string, itemId, authHeaders),
     {
       keepPreviousData: true,
     },
--- libs/items/src/swr/variants/get-variant/service.ts
+++ libs/items/src/swr/variants/get-variant/service.ts
@@ -5,9 +5,14 @@ import { variantDetailsResponse } from './types';
 export async function getVariantDetails(
   variantId: string,
   itemId: string,
+  headers?: Record<string, string>,
 ): Promise<variantDetailsResponse> {
   const url = `/api/items/${itemId}/variants/${encodeURIComponent(variantId)}`;
-  const r = await fetch(url);
+  const r = await fetch(url, {
+    headers: {
+      ...(headers ?? {}),
+    },
+  });
   if (!r.ok) {
     await handleVestidoErrorResponse(r);
   }
