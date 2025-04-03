46b5efacaf1c9811692b84b0e5c10c8d975eab6c
Mohammed Fabinsha <fabinsha13@gmail.com>
2025-04-03 22:19:38 +0530

    wip: VN Logo In Razorpay UI

--- third-party/razorpay/src/swr/razorpay-order/create-payment/service.ts
+++ third-party/razorpay/src/swr/razorpay-order/create-payment/service.ts
@@ -17,7 +17,7 @@ export async function createRazorpayPayment(
       amount: args.amount,
       currency: 'INR',
       name: 'Vestido Nation',
-      image: 'https://beta.vestidonation.com/assets/VN-logo-white.png',
+      image: 'https://beta.vestidonation.com/assets/vn-logo-white.png',
       description: 'Your style. Your Statement.',
       order_id: args.razorpayOrderId,
       handler: async (r: RazorpayResponse) => {
