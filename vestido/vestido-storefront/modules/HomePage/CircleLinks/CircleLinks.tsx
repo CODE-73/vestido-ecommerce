3c8f56af9616413483f1beabb8b8f9fbe50c8d5e
Fathima Sayeeda <sayeeda.61911@gmail.com>
2025-04-28 17:02:10 +0530

    fix: Circle Links Top Margin in Small Screens

--- vestido/vestido-storefront/modules/HomePage/CircleLinks/CircleLinks.tsx
+++ vestido/vestido-storefront/modules/HomePage/CircleLinks/CircleLinks.tsx
@@ -9,7 +9,7 @@ export const CircleLinks: FC = () => {
   const circle_links = home_data?.circle_links;
 
   return (
-    <div className="w-full lg:w-auto overflow-x-auto no-scrollbar pt-4 sm:pt-4 sm:pb-5 lg:pb-auto lg:pt-10">
+    <div className="w-full lg:w-auto overflow-x-auto no-scrollbar pt-4 mt-6 sm:mt-auto sm:pt-4 sm:pb-5 lg:pb-auto lg:pt-10">
       <div className="flex space-x-4 lg:space-x-2 px-4 lg:justify-center">
         {circle_links?.map((card, index) => (
           <CircleLink key={index} data={card} />
