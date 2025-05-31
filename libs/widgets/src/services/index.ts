ecc84f27d0195adc949609445c0466df857a4199
Mohammed Fabinsha <fabinsha13@gmail.com>
2025-05-31 19:02:12 +0530

    wip: Period Over Period API

--- libs/widgets/src/services/index.ts
+++ libs/widgets/src/services/index.ts
@@ -1,5 +1,6 @@
 export * from './average-order-value';
 export * from './order-count-trend';
+export * from './period-over-period';
 export * from './revenue-by-category';
 export * from './revenue-over-time';
 export * from './zod';
