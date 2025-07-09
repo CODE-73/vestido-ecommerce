1fde5a80a0fe8c677993da8a44128b2b4c68c405
Fathima Sayeeda <sayeeda.61911@gmail.com>
2025-07-09 11:46:53 +0530

    fix: authLoader in ProtectedRoute

--- libs/auth/src/components/ProtectedRoute.tsx
+++ libs/auth/src/components/ProtectedRoute.tsx
@@ -4,14 +4,15 @@ import { useRouter } from 'next/router';
 import { useAuth } from '../providers';
 
 export const ProtectedRoute = ({ children }: { children: ReactNode }) => {
-  const { isAuthenticated, loginRoute } = useAuth();
+  const { isAuthenticated, loginRoute, authLoaded } = useAuth();
   const router = useRouter();
 
   useEffect(() => {
+    if (!authLoaded) return;
     if (!isAuthenticated) {
       router.push(loginRoute);
     }
-  }, [isAuthenticated, loginRoute, router]);
+  }, [authLoaded, isAuthenticated, loginRoute, router]);
 
   if (!isAuthenticated) {
     return null;
