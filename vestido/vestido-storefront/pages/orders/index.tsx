e6ec3882cbc8d08fd1515e4379be66c804c596e3
Fathima Sayeeda <sayeeda.61911@gmail.com>
2025-07-10 06:48:01 +0530

    fix: OrderPage SWRConfig  Removed

--- vestido/vestido-storefront/pages/orders/index.tsx
+++ vestido/vestido-storefront/pages/orders/index.tsx
@@ -1,13 +1,8 @@
 import { NextPage } from 'next';
-import { SWRConfig } from 'swr';
 
 import OrderListView from '../../modules/orders/order-list/order-list';
 
 const AllOrdersPage: NextPage = () => {
-  return (
-    <SWRConfig>
-      <OrderListView />;
-    </SWRConfig>
-  );
+  return <OrderListView />;
 };
 export default AllOrdersPage;
