4a0cc83972cdc6dab07c06cbec65a3c78dff14d5
Fathima Sayeeda <sayeeda.61911@gmail.com>
2025-07-30 15:59:00 +0530

    fix: Auth Analytics Frontend

---
+++ vestido/vestido-dashboard/modules/widgets/widgets/signups-count-widget.tsx
@@ -0,0 +1,55 @@
+import { Card, CardContent } from '@vestido-ecommerce/shadcn-ui/card';
+import { BaseReportFilter } from '@vestido-ecommerce/widgets';
+import { useAuthAnalytics } from '@vestido-ecommerce/widgets/client';
+
+import { type GroupBy, isValidGroupBy } from '../formatters';
+import { InjectedWidgetProps, WidgetWrapper } from './widget-wrapper';
+
+type SignupsWidgetProps = InjectedWidgetProps<{
+  groupBy: GroupBy;
+}>;
+
+const SignupCountWidget: React.FC = () => {
+  const defaultFilters = { groupBy: 'daily' };
+  return (
+    <WidgetWrapper widgetId="signups" defaultFilters={defaultFilters}>
+      {/* @ts-expect-error Props are filled by the WidgetWrapper */}
+      <SignupsWidgetDisplay />
+    </WidgetWrapper>
+  );
+};
+
+const SignupsWidgetDisplay: React.FC<SignupsWidgetProps> = ({
+  fromDate,
+  toDate,
+  widgetFilters: { groupBy: rawGroupBy = 'daily' } = { groupBy: 'daily' },
+}) => {
+  const groupBy: GroupBy = isValidGroupBy(rawGroupBy!) ? rawGroupBy : 'daily';
+
+  const filter: BaseReportFilter = {
+    fromDate,
+    toDate,
+    groupBy,
+  };
+
+  const { data: signup_data } = useAuthAnalytics(filter);
+  const signup_count = signup_data?.data?.reduce((sum, item) => {
+    return sum + Number(item.signup_count);
+  }, 0);
+  console.log('signupcount', signup_count);
+
+  return (
+    <Card className="w-full h-full pt-2">
+      <CardContent>
+        <h4 className="text-xs text-tremor-default text-tremor-content dark:text-dark-tremor-content">
+          New Users
+        </h4>
+        <p className="text-tremor-metric font-semibold text-tremor-content-strong dark:text-dark-tremor-content-strong">
+          {signup_count ?? 0}
+        </p>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default SignupCountWidget;
