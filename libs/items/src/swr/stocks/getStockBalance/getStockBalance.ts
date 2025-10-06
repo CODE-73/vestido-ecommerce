55a60bf347bb5609b502c02a27af703019a2688b
Fathima Sayeeda <sayeeda.61911@gmail.com>
2025-10-06 14:17:20 +0530

    fix: Hook Naming

--- libs/items/src/swr/stocks/getStockBalance/getStockBalance.ts
+++ libs/items/src/swr/stocks/getStockBalance/getStockBalance.ts
@@ -6,7 +6,7 @@ import { GetStockBalanceResponse } from 'libs/items/src/services/stocks';
 import { StockSWRKeys } from '../keys';
 import { getStockBalance } from './service';
 
-export function useStockBal(args: string | null) {
+export function useStockBalance(args: string | null) {
   const { authHeaders } = useAuth();
   const key = [StockSWRKeys.STOCKBALANCE, StockSWRKeys.GET, args];
   return useSWRImmutable<GetStockBalanceResponse, Error>(
